2d83ae03e60decddb0d761ea43f1cd521c7c49d6
indradio <indradio@users.noreply.github.com>
2019-08-27 16:37:12 +0700

    upload

--- application/controllers/Lembur.php
+++ application/controllers/Lembur.php
@@ -14,7 +14,6 @@ class Lembur extends CI_Controller
         $data['sidemenu'] = 'Lembur';
         $data['sidesubmenu'] = 'LemburKu';
         $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
-        $data['pendapatan'] = $this->db->get('pendapatan')->result_array();
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/navbar', $data);
@@ -22,4 +21,81 @@ class Lembur extends CI_Controller
         $this->load->view('templates/footer');
     }
 
+    public function rencana()
+    {
+        $data['sidemenu'] = 'Lembur';
+        $data['sidesubmenu'] = 'Rencana';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['lembur'] = $this->db->get_where('lembur', ['npk' =>  $this->session->userdata('npk')])->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('lembur/rencana', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function realisasi()
+    {
+        $data['sidemenu'] = 'Lembur';
+        $data['sidesubmenu'] = 'Realisasi';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['lembur'] = $this->db->get_where('lembur', ['npk' =>  $this->session->userdata('npk')])->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('lembur/realisasi', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function tambah()
+    {
+        date_default_timezone_set('asia/jakarta');
+        $karyawan = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $atasan1 = $this->db->get_where('karyawan', ['npk' => $this->session->userdata('atasan1')])->row_array();
+        $atasan2 = $this->db->get_where('karyawan', ['npk' => $this->session->userdata('atasan2')])->row_array();
+
+        $queryLembur = "SELECT COUNT(*)
+        FROM `lembur`
+        WHERE YEAR(tglpengajuan) = YEAR(CURDATE())
+        ";
+        $lembur = $this->db->query($queryLembur)->row_array();
+        $totalLembur = $lembur['COUNT(*)'] + 1;
+
+        $mulai = strtotime($this->input->post('tglmulai'));
+        $selesai = strtotime($this->input->post('tglselesai'));
+        $durasi = $selesai - $mulai;
+        $jam   = floor($durasi / (60 * 60));
+        $menit = $durasi - $jam * (60 * 60);
+        $menit = floor($menit / 60);
+
+        $data = [
+            'id' => 'OT' . date('y') . $totalLembur,
+            'tglpengajuan' => date('Y-m-d H:i:s'),
+            'jenis_lembur' => $this->input->post('jenis'),
+            'npk' => $this->session->userdata('npk'),
+            'nama' => $karyawan['nama'],
+            'tglmulai' => $this->input->post('tglmulai'),
+            'tglselesai' => $this->input->post('tglselesai'),
+            'atasan1' => $atasan1['inisial'],
+            'atasan2' => $atasan2['inisial'],
+            'durasi' => $jam . ' jam ' . $menit . ' menit',
+            'status' => '1'
+        ];
+        $this->db->insert('lembur', $data);
+
+        redirect('lembur/rencana_aktivitas');
+    }
+
+    public function rencana_activitas()
+    {
+        $data['sidemenu'] = 'Lembur';
+        $data['sidesubmenu'] = 'Rencana';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['lembur'] = $this->db->get_where('lembur', ['npk' =>  $this->session->userdata('npk')])->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('lembur/rencana_aktivitas', $data);
+        $this->load->view('templates/footer');
+    }
 }
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -337,6 +337,23 @@
         });
     });
 </script>
+<!-- javascript for init -->
+<script>
+    $('.datetimepicker').datetimepicker({
+        format: 'Y-M-D HH:mm',
+        icons: {
+            time: "fa fa-clock-o",
+            date: "fa fa-calendar",
+            up: "fa fa-chevron-up",
+            down: "fa fa-chevron-down",
+            previous: 'fa fa-chevron-left',
+            next: 'fa fa-chevron-right',
+            today: 'fa fa-screenshot',
+            clear: 'fa fa-trash',
+            close: 'fa fa-remove'
+        }
+    });
+</script>
 
 </body>
 
