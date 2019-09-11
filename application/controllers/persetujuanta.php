1d31b8003a05ee8855d4266976bfd2ad442e4f2a
indradio <indradio@users.noreply.github.com>
2019-09-11 18:25:39 +0700

    Update TA

--- application/controllers/persetujuanta.php
+++ application/controllers/persetujuanta.php
@@ -48,7 +48,7 @@ class Persetujuanta extends CI_Controller
             $ga_admin = $this->db->get('karyawan_admin')->row_array();
             $my_apikey = "NQXJ3HED5LW2XV440HCG";
             $destination = $ga_admin['phone'];
-            $message = "*PENGAJUAN PERJALANAN DINAS TA*\r\n \r\n No. Reservasi : *" . $rsv['id'] . "*" .
+            $message = "*PENGAJUAN PERJALANAN DINAS TA/TAPP*\r\n \r\n No. Reservasi : *" . $rsv['id'] . "*" .
                 "\r\n Nama : *" . $rsv['nama'] . "*" .
                 "\r\n Tujuan : *" . $rsv['tujuan'] . "*" .
                 "\r\n Keperluan : *" . $rsv['keperluan'] . "*" .
