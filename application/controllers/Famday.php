a6fd568b3b50f8d724bab72a32782590a36bd3c8
indradio <indradio@users.noreply.github.com>
2019-10-07 07:42:54 +0700

    Update

--- application/controllers/Famday.php
+++ application/controllers/Famday.php
@@ -9,18 +9,18 @@ class Famday extends CI_Controller
         is_logged_in();
     }
 
-    // public function index()
-    // {
-    //     $data['sidemenu'] = 'Family Day';
-    //     $data['sidesubmenu'] = 'Daftar & Vote';
-    //     $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
-    //     $data['famday'] = $this->db->get_where('famday', ['npk' =>  $this->session->userdata('npk')])->result_array();
-    //     $this->load->view('templates/header', $data);
-    //     $this->load->view('templates/sidebar', $data);
-    //     $this->load->view('templates/navbar', $data);
-    //     $this->load->view('famday/index', $data);
-    //     $this->load->view('templates/footer');
-    // }
+    public function index()
+    {
+        $data['sidemenu'] = 'Family Day';
+        $data['sidesubmenu'] = 'Daftar & Vote';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['famday'] = $this->db->get_where('famday', ['npk' =>  $this->session->userdata('npk')])->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('famday/index', $data);
+        $this->load->view('templates/footer');
+    }
 
     public function vote()
     {
@@ -57,5 +57,27 @@ class Famday extends CI_Controller
 
         $this->session->set_flashdata('message', 'vote');
         redirect('dashboard');
+    }
+
+    public function daftar()
+    {
+        $total = $this->input->post('ikut') + $this->input->post('pasangan') + $this->input->post('anak1') + $this->input->post('anak2') + $this->input->post('anak3') + $this->input->post('tambahan');
+        $data = [
+            'npk' => $this->session->userdata('npk'),
+            'nama' => $this->input->post('nama'),
+            'ikut' => $this->input->post('ikut'),
+            'ukuran' => $this->input->post('baju'),
+            'pasangan' => $this->input->post('pasangan'),
+            'ukuran_pasangan' => $this->input->post('pasangan_baju'),
+            'anak1' => $this->input->post('anak1'),
+            'anak2' => $this->input->post('anak2'),
+            'anak3' => $this->input->post('anak3'),
+            'tambahan' => $this->input->post('tambahan'),
+            'total' => $total
+        ];
+        $this->db->insert('famday', $data);
+
+        $this->session->set_flashdata('message', 'vote');
+        redirect('famday');
     }
 }
--- application/views/famday/index.php
+++ application/views/famday/index.php
@@ -19,7 +19,6 @@
                             <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
                                 <thead>
                                     <tr>
-                                        <th>Vote</th>
                                         <th>Ikut</th>
                                         <th>Ukuran Baju</th>
                                         <th>Istri/Suami</th>
@@ -33,8 +32,7 @@
                                 </thead>
                                 <tfoot>
                                     <tr>
-                                    <th>Vote</th>
-                                    <th>Ikut</th>
+                                        <th>Ikut</th>
                                         <th>Ukuran Baju</th>
                                         <th>Istri/Suami</th>
                                         <th>Ukuran Baju</th>
@@ -49,7 +47,6 @@
                                     <?php
                                     foreach ($famday as $f) : ?>
                                         <tr>
-                                            <td><?= $f['vote']; ?></td>
                                             <td><?= $f['ikut']; ?></td>
                                             <td><?= $f['ukuran']; ?></td>
                                             <td><?= $f['pasangan']; ?></td>
@@ -57,6 +54,7 @@
                                             <td><?= $f['anak1']; ?></td>
                                             <td><?= $f['anak2']; ?></td>
                                             <td><?= $f['anak3']; ?></td>
+                                            <td><?= $f['tambahan']; ?></td>
                                         </tr>
                                     <?php endforeach; ?>
                                 </tbody>
@@ -87,7 +85,7 @@
                     </div>
                 </div>
                 <div class="modal-body">
-                    <form class="form" method="post" action="<?= base_url('cekdl/edit'); ?>">
+                    <form class="form" method="post" action="<?= base_url('famday/daftar'); ?>">
                         <div class="card-body">
                             <div class="row">
                                 <label class="col-md-4 col-form-label">Nama Karyawan</label>
@@ -100,14 +98,14 @@
                             <div class="row">
                                 <label class="col-md-4 col-form-label">Keikutsertaan</label>
                                 <div class="form-check">
-    <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" value="1">                                 
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
-</div>
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox" id="ikut" name="ikut" value="1">
+                                        <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
                             <div class="row">
                                 <label class="col-md-4 col-form-label">Ukuran Baju</label>
                                 <div class="col-md-6">
@@ -123,14 +121,14 @@
                             <div class="row">
                                 <label class="col-md-4 col-form-label">Istri/Suami</label>
                                 <div class="form-check">
-    <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" value="1">                                 
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
-</div>
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox" id="pasangan" name="pasangan" value="1">
+                                        <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
                             <div class="row">
                                 <label class="col-md-4 col-form-label">Ukuran Baju</label>
                                 <div class="col-md-6">
@@ -144,43 +142,61 @@
                                 </div>
                             </div>
                             <div class="row">
-                            <label class="col-md-4 col-form-label">Anak</label>
-                            <div class="form-check">
-    <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" value="1">
-       Anak Ke-1                                    
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
-</div>
-<div class="row">
-<label class="col-md-4 col-form-label"></label>
-                            <div class="form-check">
-    <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" value="1">
-       Anak Ke-2                                      
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
-</div>
-<div class="row">
-<label class="col-md-4 col-form-label"></label>
-<div class="form-check">
-    <label class="form-check-label">
-        <input class="form-check-input" type="checkbox" value="1">
-       Anak Ke-3                              
-        <span class="form-check-sign">
-            <span class="check"></span>
-        </span>
-    </label>
-</div>
+                                <label class="col-md-4 col-form-label">Anak</label>
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox" id="anak1" name="anak1" value="1">
+                                        Anak Ke-1
+                                        <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-md-4 col-form-label"></label>
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox" id="anak2" name="anak2" value="1">
+                                        Anak Ke-2
+                                        <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-md-4 col-form-label"></label>
+                                <div class="form-check">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox" id="anak3" name="anak3" value="1">
+                                        Anak Ke-3
+                                        <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label class="col-md-4 col-form-label">Keluarga Tambahan</label>
+                                <div class="col-md-6">
+                                    <select class="selectpicker" id="tambahan" name="tambahan" data-style="select-with-transition" title="Pilih" data-size="7" data-width="75%" data-live-search="false" required>
+                                        <option value="1">1 Orang</option>
+                                        <option value="2">2 Orang</option>
+                                        <option value="3">3 Orang</option>
+                                        <option value="4">4 Orang</option>
+                                        <option value="5">5 Orang</option>
+                                        <option value="6">6 Orang</option>
+                                        <option value="7">7 Orang</option>
+                                        <option value="8">8 Orang</option>
+                                        <option value="9">9 Orang</option>
+                                        <option value="10">10 Orang</option>
+                                        <option value="10+">Lebih dari 10 Orang</option>
+                                    </select>
+                                </div>
                             </div>
                             <div class="modal-footer">
-                                <button type="submit" class="btn btn-fill btn-success">UPDATE</button>
+                                <button type="submit" class="btn btn-fill btn-success">DAFTAR</button>
                                 <button type="button" class="btn btn-fill btn-default ml-1" data-dismiss="modal">TUTUP</button>
                             </div>
                         </div>
