81255f85e5b63f4477bf39237062ddd6b7e9abc8
indradio <indradio@users.noreply.github.com>
2019-10-07 14:52:04 +0700

    Up

---
+++ application/controllers/Meeting.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Meeting extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        redirect('https://winteq-astra.com/meeting/day.php');
+    }
+}
