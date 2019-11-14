6b98dcb8e21cc4422edb6b9f2a4e0494aaaf42c9
abuhanifah4 <53813691+abuhanifah4@users.noreply.github.com>
2019-11-14 15:20:41 +0700

    Update lp_lembur_persetujuan.php

--- application/views/lembur/lp_lembur_persetujuan.php
+++ application/views/lembur/lp_lembur_persetujuan.php
@@ -9,7 +9,7 @@
             <div class="card-icon">
               <i class="material-icons">assignment</i>
             </div>
-            <h4 class="card-title">RENCANA - Menunggu Persetujuan Koordinator/Section & Depthead/RDA | <?= date("d-m-Y H:i"); ?></h4>
+            <h4 class="card-title">RENCANA - Menunggu Persetujuan Koordinator/Section & Depthead | <?= date("d-m-Y H:i"); ?></h4>
           </div>
           <div class="card-body">
           <div class="table-responsive">
