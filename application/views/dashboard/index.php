da928b18622c4b954f86b9604279dbe73d05ab6e
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2019-11-21 13:08:00 +0700

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,7 +1,7 @@
 <div class="content">
   <div class="flash-data" data-flashdata="<?= $this->session->flashdata('message'); ?>"></div>
-  <div class="alert alert-info alert-dismissible fade show" role="alert">
-  <strong>Hay Your RAISA!</strong> Punya pertanyaan tentang LEMBUR menggunakan RAISA? Cuss klik <a href="<?= base_url(); ?>assets/pdf/pertanyaan_lembur.pdf" target="_blank">DISINI</a>.
+  <div class="alert alert-rose alert-dismissible fade show" role="alert">
+  <strong>Hay Your RAISA!</strong> Mau dapet COFFEE MAKER? yuk isi survei kepuasan acara FAMILY DAY 2019? Klik <a href="<?= base_url('famday/survey'); ?>" target="_blank">DISINI</a>.
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
