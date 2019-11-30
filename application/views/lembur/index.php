2c7cc260c5d0e9711fe69a54651259fb69c6aec3
indradio <indradio@users.noreply.github.com>
2019-12-01 00:29:55 +0700

    update histiry

--- application/views/lembur/index.php
+++ application/views/lembur/index.php
@@ -174,6 +174,20 @@
                           <?php } else { ?>
                               <a href="<?= base_url('lembur/rencana_aktivitas/') . $l['id']; ?>" class="badge badge-pill badge-success">Detail</a>
                           <?php }; ?>
+                              <a href="#" class="badge badge-pill badge-info" data-toggle="modal" data-target="#historyLembur"
+                              data-tglmengajukan="<?= date('d M Y H:i', strtotime($l['tglpengajuan'])); ?>"
+                              data-atasan1="<?= $l['atasan1_rencana']; ?>"
+                              data-atasan2="<?= $l['atasan2_rencana']; ?>"
+                              data-tgl1_rencana="<?= date('d M Y H:i', strtotime($l['tgl_atasan1_rencana'])); ?>"
+                              data-tgl2_rencana="<?= date('d M Y H:i', strtotime($l['tgl_atasan2_rencana'])); ?>"
+                              data-tgl1_realisasi="<?= date('d M Y H:i', strtotime($l['tgl_atasan1_realisasi'])); ?>"
+                              data-tgl2_realisasi="<?= date('d M Y H:i', strtotime($l['tgl_atasan2_realisasi'])); ?>"
+                              data-ga="<?= $l['admin_ga']; ?>"
+                              data-hr="<?= $l['admin_hr']; ?>"
+                              data-tgl_ga="<?= date('d M Y H:i', strtotime($l['tgl_admin_ga'])); ?>"
+                              data-tgl_hr="<?= date('d M Y H:i', strtotime($l['tgl_admin_hr'])); ?>"
+                              data-status="<?= $l['status']; ?>"
+                              >history</a>
                       </td>
                       <td class="text-right">
                           <?php if ($l['status'] == '9' ) { ?>
@@ -198,4 +212,234 @@
   </div>
 </div>
 <!-- Modal -->
-
+<div class="modal fade" id="historyLembur" tabindex="-1" role="dialog" aria-labelledby="historyLemburTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="card card-signup card-plain">
+                <div class="modal-header">
+                    <div class="card-header card-header-primary text-center">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                            <i class="material-icons">clear</i>
+                        </button>
+                        <h4 class="card-title">HISTORY LEMBUR</h4>
+                    </div>
+                </div>
+                  <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                          <div class="card card-timeline card-plain">
+                            <div class="card-body">
+                              <ul class="timeline">
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge danger">
+                                    <i class="material-icons">card_travel</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-danger">Proses HR SSC</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="admin_ssc"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="timeline-badge success">
+                                    <i class="material-icons">extension</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-success">Proses HR WTQ</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="admin_hr"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tgl_hr"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge info">
+                                    <i class="material-icons">fingerprint</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-info">Persetujuan Realisasi Atasan 2</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="atasan2_realisasi"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tgl_atasan2_realisasi"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge info">
+                                    <i class="material-icons">fingerprint</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-info">Persetujuan Realisasi Atasan 1</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="atasan1_realisasi"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tgl_atasan1_realisasi"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge info">
+                                    <i class="material-icons">fingerprint</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-info">Persetujuan Rencana Atasan 2</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="atasan2_rencana"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tgl_atasan2_rencana"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="timeline-badge success">
+                                    <i class="material-icons">extension</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-success">Mengetahui GA</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="admin_ga"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tgl_ga"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li class="timeline-inverted">
+                                  <div class="timeline-badge info">
+                                    <i class="material-icons">fingerprint</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-info">Persetujuan Rencana Atasan 1</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                    <p id="atasan1_rencana"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tgl_atasan1_rencana"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="timeline-badge warning">
+                                    <i class="material-icons">add_circle_outline</i>
+                                  </div>
+                                  <div class="timeline-panel">
+                                    <div class="timeline-heading">
+                                      <span class="badge badge-pill badge-warning">Mengajukan</span>
+                                    </div>
+                                    <div class="timeline-body">
+                                      <p id="lembur"></p>
+                                    </div>
+                                    <h6>
+                                      <i class="ti-time" id="tglmengajukan"></i>
+                                    </h6>
+                                  </div>
+                                </li>
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button class="btn btn-default btn-link" data-dismiss="modal">TUTUP</button>
+                        </div>
+                    </div>
+                  </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+        $('#historyLembur').on('show.bs.modal', function (event) {
+            var button = $(event.relatedTarget)
+            var tglmengajukan = button.data('tglmengajukan')
+            var atasan1 = button.data('atasan1')
+            var atasan2 = button.data('atasan2')
+            var tgl1_rencana = button.data('tgl1_rencana')
+            var tgl2_rencana = button.data('tgl2_rencana')
+            var tgl1_realisasi = button.data('tgl1_realisasi')
+            var tgl2_realisasi = button.data('tgl2_realisasi')
+            var ga = button.data('ga')
+            var tgl_ga = button.data('tgl_ga')
+            var hr = button.data('hr')
+            var tgl_hr = button.data('tgl_hr')
+            var status = button.data('status')
+            var modal = $(this)
+            if (status > 0){
+            document.getElementById("lembur").innerHTML = "Mengajukan lembur pada";
+            document.getElementById("tglmengajukan").innerHTML = tglmengajukan;
+            }else{
+            document.getElementById("lembur").innerHTML = "Lembur kamu telah DIBATALKAN";
+            document.getElementById("tglmengajukan").innerHTML = null;
+            }
+            if (status > 2){
+            document.getElementById("atasan1_rencana").innerHTML = "Disetujui oleh " + atasan1;
+            document.getElementById("tgl_atasan1_rencana").innerHTML = tgl1_rencana;
+            }else{
+            document.getElementById("atasan1_rencana").innerHTML = "Menunggu Persetujuan " + atasan1;
+            document.getElementById("tgl_atasan1_rencana").innerHTML = null;
+            }
+            if (status > 3){
+            document.getElementById("atasan2_rencana").innerHTML = "Disetujui oleh " + atasan2;
+            document.getElementById("tgl_atasan2_rencana").innerHTML = tgl2_rencana;
+            }else{
+            document.getElementById("atasan2_rencana").innerHTML = "Menunggu Persetujuan " + atasan2;
+            document.getElementById("tgl_atasan2_rencana").innerHTML = null;
+            }
+            if (status > 5){
+            document.getElementById("atasan1_realisasi").innerHTML = "Disetujui oleh " + atasan1;
+            document.getElementById("tgl_atasan1_realisasi").innerHTML = tgl1_realisasi;
+            }else{
+            document.getElementById("atasan1_realisasi").innerHTML = "Menunggu Persetujuan " + atasan1;
+            document.getElementById("tgl_atasan1_realisasi").innerHTML = null;
+            }
+            if (status > 6){
+            document.getElementById("atasan2_realisasi").innerHTML = "Disetujui oleh " + atasan2;
+            document.getElementById("tgl_atasan2_realisasi").innerHTML = tgl2_realisasi;
+            }else{
+            document.getElementById("atasan2_realisasi").innerHTML = "Menunggu Persetujuan " + atasan2;
+            document.getElementById("tgl_atasan2_realisasi").innerHTML = null;
+            }
+            if (status > 7){
+            document.getElementById("admin_hr").innerHTML = "Diproses oleh " + hr;
+            document.getElementById("tgl_hr").innerHTML = tgl_hr;
+            document.getElementById("admin_ssc").innerHTML = "Fitur ini masih dalam pengembangan";
+            }else{
+            document.getElementById("admin_hr").innerHTML = "Menunggu proses HR ";
+            document.getElementById("tgl_hr").innerHTML = null;
+            document.getElementById("admin_ssc").innerHTML = "Fitur ini masih dalam pengembangan";
+            }
+            if (ga){
+            document.getElementById("admin_ga").innerHTML = "Diketahui oleh " + ga;
+            document.getElementById("tgl_ga").innerHTML = tgl_ga;
+            }else{
+            document.getElementById("admin_ga").innerHTML = "Lembur kamu BELUM/TIDAK diketahui oleh GA";
+            document.getElementById("tgl_ga").innerHTML = null;
+            }
+        })
+    });
+</script>
