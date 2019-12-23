34fdac9e30a99e41df9f5d03cc6ce245c183ee9b
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2019-12-23 10:47:16 +0700

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,13 +1,11 @@
 <div class="content">
   <div class="flash-data" data-flashdata="<?= $this->session->flashdata('message'); ?>"></div>
-  <!-- <div class="alert alert-rose alert-dismissible fade show" role="alert">
-    <strong>Hay Your RAISA!</strong> Buat kamu yang penikmat kopi, Mau dapet COFFEE MAKER biar gak repot nyeduh? Gampangg! 
-    </br>yuk isi survei kepuasan acara FAMILY DAY 2019 dan kamu punya kesempatan untuk mendapatkan COFFEE MAKER keren ini? Klik <a href="<?= base_url('famday/survey'); ?>" target="_blank">DISINI</a>. 
-    </br>RAISA tunggu paling lambat <b>Tanggal 22 Nov 2019 Jam 16:30</b>
+  <div class="alert alert-rose alert-dismissible fade show" role="alert">
+    <strong>Hay Your RAISA!</strong> Mau lihat design ID CARD ? Klik <a href="<?= base_url('dashboard/idcard'); ?>" target="_blank">DISINI</a>. 
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
-  </div> -->
+  </div>
   <div class="container-fluid">
     <!-- Banner -->
     <div class="row">
