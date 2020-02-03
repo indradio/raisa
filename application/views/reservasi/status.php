cde45c3bdee2d2bb4780f52ab862dc56ae9b15fc
magangwinteq <58926045+magangwinteq@users.noreply.github.com>
2020-02-03 08:45:10 +0700

    Update status.php

--- application/views/reservasi/status.php
+++ application/views/reservasi/status.php
@@ -4,8 +4,9 @@
         <div class="row">
             <div class="col-lg-8 ml-auto mr-auto">
                 <ul class="timeline timeline-simple">
+                    <a href="<?= base_url('reservasi')?>" class="btn btn-sm ">Kembali</a>
                     <?php if ($reservasi['status'] == 9) { ?>
-                        <li class="timeline-inverted">
+                       <li class="timeline-inverted">
                             <div class="timeline-badge info">
                                 <i class="material-icons">card_travel</i>
                             </div>
@@ -304,12 +305,12 @@
                                 <span class="badge badge-pill badge-primary">Pengajuan Perjalanan Dinas</span>
                             </div>
                             <div class="timeline-body">
-                                <p>Jenis Perjalanan : <?= $reservasi['jenis_perjalanan']; ?></p>
-                                <p>Kendaraan : <?= $reservasi['nopol'] . ' (' . $reservasi['kepemilikan'] . ')'; ?></p>
-                                <p>Tujuan : <?= $reservasi['tujuan']; ?></p>
-                                <p>Keperluan : <?= $reservasi['keperluan']; ?></p>
-                                <p>Estimasi Waktu Keberangkatan : <?= date('d/m/Y', strtotime($reservasi['tglberangkat'])); ?> <?= date('H:i', strtotime($reservasi['jamberangkat'])); ?></p>
-                                <p>Estimasi Waktu Kembali : <?= date('d/m/Y', strtotime($reservasi['tglkembali'])); ?> <?= date('H:i', strtotime($reservasi['jamkembali'])); ?></p>
+                                <p>Jenis Perjalanan : <strong><?= $reservasi['jenis_perjalanan']; ?></strong></p>
+                                <p>Kendaraan : <strong><?= $reservasi['nopol'] . ' (' . $reservasi['kepemilikan'] . ')'; ?></strong></p>
+                                <p>Tujuan : <strong><?= $reservasi['tujuan']; ?></strong></p>
+                                <p>Keperluan : <strong><?= $reservasi['keperluan']; ?></strong></p>
+                                <p>Estimasi Waktu Keberangkatan : <strong><?= date('d/m/Y', strtotime($reservasi['tglberangkat'])); ?> <?= date('H:i', strtotime($reservasi['jamberangkat'])); ?></strong></p>
+                                <p>Estimasi Waktu Kembali : <strong><?= date('d/m/Y', strtotime($reservasi['tglkembali'])); ?> <?= date('H:i', strtotime($reservasi['jamkembali'])); ?></strong></p>
                             </div>
                             <h6>
                                 <i class="ti-time"></i> <?= date('d/m/Y H:i', strtotime($reservasi['tglreservasi'])); ?>
