5163adf6add822249d0397527b7b4644b741298f
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-02-27 16:27:48 +0700

    Update Persetujuanta.php

--- application/controllers/Persetujuanta.php
+++ application/controllers/Persetujuanta.php
@@ -44,13 +44,38 @@ class Persetujuanta extends CI_Controller
         date_default_timezone_set('asia/jakarta');
         $rsv = $this->db->get_where('reservasi', ['id' =>  $this->input->post('id')])->row_array();
         if ($this->session->userdata('posisi_id') == '2') {
-     
+            if ($rsv['jenis_perjalanan']=='TA'){
                 $this->db->set('div_ttd', "Disetujui oleh " . $this->session->userdata['inisial']);
                 $this->db->set('tgl_div', date('Y-m-d H:i:s'));
                 $this->db->set('status', '5');
                 $this->db->where('id', $this->input->post('id'));
                 $this->db->update('reservasi');
-
+    
+                $this->db->where('sect_id', '214');
+                $hr_admin = $this->db->get('karyawan_admin')->row_array();
+                $my_apikey = "NQXJ3HED5LW2XV440HCG";
+                $destination = $hr_admin['phone'];
+                $message = "*PENGAJUAN PERJALANAN DINAS TA/TAPP*\r\n \r\n No. Reservasi : *" . $rsv['id'] . "*" .
+                    "\r\n Nama : *" . $rsv['nama'] . "*" .
+                    "\r\n Tujuan : *" . $rsv['tujuan'] . "*" .
+                    "\r\n Keperluan : *" . $rsv['keperluan'] . "*" .
+                    "\r\n Peserta : *" . $rsv['anggota'] . "*" .
+                    "\r\n Berangkat : *" . $rsv['tglberangkat'] . "* *" . $rsv['jamberangkat'] . "* _estimasi_" .
+                    "\r\n Kembali : *" . $rsv['tglkembali'] . "* *" . $rsv['jamkembali'] . "* _estimasi_" .
+                    "\r\n Kendaraan : *" . $rsv['nopol'] . "* ( *" . $rsv['kepemilikan'] . "*" .
+                    " ) \r\n \r\nPerjalanan ini membutuhkan persetujuan dari anda. Untuk informasi lebih lengkap silahkan buka portal aplikasi di link berikut https://raisa.winteq-astra.com";
+                $api_url = "http://panel.apiwha.com/send_message.php";
+                $api_url .= "?apikey=" . urlencode($my_apikey);
+                $api_url .= "&number=" . urlencode($destination);
+                $api_url .= "&text=" . urlencode($message);
+                json_decode(file_get_contents($api_url, false));
+            }elseif ($rsv['jenis_perjalanan']=='TAPP'){
+                $this->db->set('div_ttd', "Disetujui oleh " . $this->session->userdata['inisial']);
+                $this->db->set('tgl_div', date('Y-m-d H:i:s'));
+                $this->db->set('status', '6');
+                $this->db->where('id', $this->input->post('id'));
+                $this->db->update('reservasi');
+    
                 $this->db->where('sect_id', '215');
                 $hr_admin = $this->db->get('karyawan_admin')->row_array();
                 $my_apikey = "NQXJ3HED5LW2XV440HCG";
@@ -69,7 +94,7 @@ class Persetujuanta extends CI_Controller
                 $api_url .= "&number=" . urlencode($destination);
                 $api_url .= "&text=" . urlencode($message);
                 json_decode(file_get_contents($api_url, false));
-
+            }
         } elseif ($this->session->userdata('posisi_id') == '3') {
             $this->db->set('fin_ttd', "Disetujui oleh " . $this->session->userdata['inisial']);
             $this->db->set('tgl_fin', date('Y-m-d H:i:s'));
