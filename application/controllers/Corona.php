60cb9a88a04e980eef04416adb7ab258ba088d95
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-03-17 17:07:03 +0700

    update corona

---
+++ application/controllers/Corona.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Corona extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+        $this->load->model("Karyawan_model");
+    }
+
+    public function index()
+    {
+        $data['sidemenu'] = 'Info COVID-19';
+        $data['sidesubmenu'] = 'Daftar Tamu';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['visit'] = $this->db->get('visit')->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('corona/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function protokol()
+    {
+        $data['sidemenu'] = 'Info COVID-19';
+        $data['sidesubmenu'] = 'Protokol';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['visit'] = $this->db->get('visit')->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('corona/protokol', $data);
+        $this->load->view('templates/footer');
+    }
+    
+    public function tamu()
+    {
+        $data['sidemenu'] = 'Info COVID-19';
+        $data['sidesubmenu'] = 'Daftar Tamu';
+        $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
+        $data['visit'] = $this->db->get('visit')->result_array();
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('corona/tamu', $data);
+        $this->load->view('templates/footer');
+    }
+    
+}
