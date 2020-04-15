c8643a00580f3a1fe91e505a07b546cd29d81989
indradio <indradio@users.noreply.github.com>
2020-04-15 13:06:12 +0700

    Update

--- application/controllers/Depthead.php
+++ application/controllers/Depthead.php
@@ -25,12 +25,29 @@ class Depthead extends CI_Controller
     public function presensi()
     {
         date_default_timezone_set('asia/jakarta');
-        if (empty($this->input->post('month'))) {
-            $data['bulan'] = date('m');
+        $fr  = date('Y-m-d 00:00:00', strtotime($this->input->post('datefr')));
+        $to = date('Y-m-d 23:59:59', strtotime($this->input->post('dateto')));
+        if ($fr != null and $to != null) {
+            $this->db->where('time >=', $fr);
+            $this->db->where('time <=', $to);
+            // $this->db->where('dept_id', $this->session->userdata('dept_id'));
+            $data['presensi'] = $this->db->get('presensi')->result_array();
+            $data['fr']  = date('d M Y', strtotime($this->input->post('datefr')));
+            $data['to'] = date('d M Y', strtotime($this->input->post('dateto')));
         } else {
-            $data['bulan'] = $this->input->post('month');
+            $this->db->where('time >=', date('Y-m-d 00:00:00'));
+            $this->db->where('time <=', date('Y-m-d 00:00:00'));
+            $this->db->where('dept_id', $this->session->userdata('dept_id'));
+            $data['presensi'] = $this->db->get('presensi')->result_array();
+            $data['fr']  = date('d M Y');
+            $data['to'] = date('d M Y');
         }
-        $data['tahun'] = date('Y');
+        // if (empty($this->input->post('month'))) {
+        //     $data['bulan'] = date('m');
+        // } else {
+        //     $data['bulan'] = $this->input->post('month');
+        // }
+        // $data['tahun'] = date('Y');
         $data['sidemenu'] = 'Kepala Departemen';
         $data['sidesubmenu'] = 'Laporan Kehadiran';
         $data['karyawan'] = $this->db->get_where('karyawan', ['npk' =>  $this->session->userdata('npk')])->row_array();
@@ -40,5 +57,4 @@ class Depthead extends CI_Controller
         $this->load->view('depthead/presensi', $data);
         $this->load->view('templates/footer');
     }
-    
 }
--- application/views/depthead/presensi.php
+++ application/views/depthead/presensi.php
@@ -7,14 +7,35 @@
           <div class="card-header card-header-info card-header-icon">
             <div class="card-text">
               <h4 class="card-title">Presensi</h4>
-              <p class="card-category">Periode <?= date('F Y', strtotime("$tahun-$bulan-01")); ?></p>
+              <p class="card-category">Periode <?= $fr . ' - ' . $to; ?></p>
             </div>
           </div>
           <div class="card-body">
             <div class="toolbar">
-              <form class="form" method="post" action="<?= base_url('hr/presensi'); ?>">
+              <form class="form" action="<?= base_url('depthead/presensi'); ?>" method="post">
+                <div class="row">
+                  <label class="col-md-1 col-form-label">Dari</label>
+                  <div class="col-md-2">
+                    <div class="form-group has-default">
+                      <input type="text" class="form-control datepicker" id="datefr" name="datefr" required="true" />
+                    </div>
+                  </div>
+                  <label class="col-md-1 col-form-label">Sampai</label>
+                  <div class="col-md-2">
+                    <div class="form-group has-default">
+                      <input type="text" class="form-control datepicker" id="dateto" name="dateto" onchange='this.form.submit()' required="true" />
+                    </div>
+                  </div>
+                  <div class="col-md-1">
+                    <button type="submit" class="btn btn-twitter"><i class="material-icons">search</i> Cari</button>
+                  </div>
+                  <div class="col-md-5"></div>
+                </div>
+              </form>
+
+              <!-- <form class="form" method="post" action="<?= base_url('depthead/presensi'); ?>">
                 <div class="form-group">
-                  <!-- <label for="copro">Project*</label> -->
+                  <label for="copro">Project*</label>
                   <select class="selectpicker" data-style="btn btn-link" id="month" name="month" title="Pilih Bulan" onchange='this.form.submit()' data-size="7" data-live-search="true" required>
                     <option value="01">Januari</option>
                     <option value="02">Februari</option>
@@ -30,7 +51,7 @@
                     <option value="12">Desember</option>
                   </select>
                 </div>
-              </form>
+              </form> -->
             </div>
             <div class="material-datatables">
               <div class="table-responsive">
@@ -49,11 +70,7 @@
                   </thead>
                   <tbody>
                     <?php
-                      $this->db->where('year(time)', $tahun);
-                      $this->db->where('month(time)', $bulan);
-                      $this->db->where('dept_id', $this->session->userdata('dept_id'));
-                      $presensi = $this->db->get('presensi')->result_array();
-                      foreach ($presensi as $p) :
+                    foreach ($presensi as $p) :
                       if (date('D', strtotime($p['time'])) == 'Sat' or date('D', strtotime($p['time'])) == 'Sun') {
                         echo '<tr class="table-danger">';
                       } else {
@@ -64,23 +81,23 @@
                       echo '<th>' . $p['nama'] . '</th>';
                       echo '<th>' . date('H:i', strtotime($p['time'])) . '</th>';
 
-                        if ($p['state']=='C/In'){
-                            echo '<th><a href="#" class="badge badge-success">' . $p['state'] . '</a></th>';
-                        }elseif($p['state']=='C/Rest'){
-                            echo '<th><a href="#" class="badge badge-warning">' . $p['state'] . '</a></th>';
-                        }elseif($p['state']=='C/Out'){
-                            echo '<th><a href="#" class="badge badge-danger">' . $p['state'] . '</a></th>';
-                        }else{
-                            echo '<th></th>';
-                        }
+                      if ($p['state'] == 'C/In') {
+                        echo '<th><a href="#" class="badge badge-success">' . $p['state'] . '</a></th>';
+                      } elseif ($p['state'] == 'C/Rest') {
+                        echo '<th><a href="#" class="badge badge-warning">' . $p['state'] . '</a></th>';
+                      } elseif ($p['state'] == 'C/Out') {
+                        echo '<th><a href="#" class="badge badge-danger">' . $p['state'] . '</a></th>';
+                      } else {
+                        echo '<th></th>';
+                      }
 
                       echo '<th>' . $p['new_state'] . '</th>';
-                        if ($p['loc']){
-                            echo '<th><a href="https://www.google.com/maps/search/?api=1&query='. $p['lat'] . ',' . $p['lng'] .'" class="text-secondary" target="_blank"><u>' . $p['loc'] . '</u></a></th>';
-                        }else{
-                            echo '<th></th>';
-                        }
-                        echo '<th>' . $p['platform'] . '</th>';
+                      if ($p['loc']) {
+                        echo '<th><a href="https://www.google.com/maps/search/?api=1&query=' . $p['lat'] . ',' . $p['lng'] . '" class="text-secondary" target="_blank"><u>' . $p['loc'] . '</u></a></th>';
+                      } else {
+                        echo '<th></th>';
+                      }
+                      echo '<th>' . $p['platform'] . '</th>';
                     endforeach;
                     ?>
                   </tbody>
@@ -104,41 +121,48 @@
   $(document).ready(function() {
     var groupColumn = 0;
     var table = $('#dt-presensi').DataTable({
-        "columnDefs": [
-            { "visible": false, "targets": groupColumn }
-        ],
-        "orderFixed": [[ groupColumn, 'asc' ]],
-        "scrollY": "867px",
-        "scrollX": true,
-        "scrollCollapse": true,
-        "paging": false,
-        // "displayLength": 25,
-        "drawCallback": function ( settings ) {
-            var api = this.api();
-            var rows = api.rows( {page:'current'} ).nodes();
-            var last=null;
- 
-            api.column(groupColumn, {page:'current'} ).data().each( function ( group, i ) {
-                if ( last !== group ) {
-                    $(rows).eq( i ).before(
-                        '<tr class="group"><td colspan="9">'+group+'</td></tr>'
-                    );
- 
-                    last = group;
-                }
-            } );
-        }
+      "columnDefs": [{
+        "visible": false,
+        "targets": groupColumn
+      }],
+      "orderFixed": [
+        [groupColumn, 'asc']
+      ],
+      "scrollY": "867px",
+      "scrollX": true,
+      "scrollCollapse": true,
+      "paging": false,
+      // "displayLength": 25,
+      "drawCallback": function(settings) {
+        var api = this.api();
+        var rows = api.rows({
+          page: 'current'
+        }).nodes();
+        var last = null;
 
-    // $('#').DataTable({
-    //     order: [[0, 'asc']],
-    //     rowGroup: {
-    //         dataSrc: 0
-    //     },
-    //   "scrollY": "512px",
-    //   "scrollX": true,
-    //   "scrollCollapse": true,
-    //   "ordering": true,
-    //   "paging": false
+        api.column(groupColumn, {
+          page: 'current'
+        }).data().each(function(group, i) {
+          if (last !== group) {
+            $(rows).eq(i).before(
+              '<tr class="group"><td colspan="9">' + group + '</td></tr>'
+            );
+
+            last = group;
+          }
+        });
+      }
+
+      // $('#').DataTable({
+      //     order: [[0, 'asc']],
+      //     rowGroup: {
+      //         dataSrc: 0
+      //     },
+      //   "scrollY": "512px",
+      //   "scrollX": true,
+      //   "scrollCollapse": true,
+      //   "ordering": true,
+      //   "paging": false
     });
   });
 </script>
