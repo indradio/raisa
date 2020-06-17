d82c66908e8d2878ff7f866619bc402868aa1107
indradio <indradio@users.noreply.github.com>
2020-06-17 15:34:18 +0700

    Update sjdlpp.php

--- application/views/perjalanandl/sjdlpp.php
+++ application/views/perjalanandl/sjdlpp.php
@@ -1,17 +1,17 @@
 <?php
 $pdf = new FPDF('L', 'mm', 'A5');
-$pdf->SetMargins(20,1,20,-5);
+$pdf->SetMargins(20, 1, 20, -5);
 
 // membuat halaman baru
-$pdf->AddFont('arial-monospaced','','arial-monospaced.php');
+$pdf->AddFont('arial-monospaced', '', 'arial-monospaced.php');
 $pdf->AddPage();
 // setting jenis font yang akan digunakan
 // mencetak string 
 
-if ($perjalanan['status']== 0){
+if ($perjalanan['status'] == 0) {
     $pdf->Image('assets/img/watermark/batal-perjalanan.png', 40, 5, 100, 70);
     $pdf->Image('assets/img/watermark/batal-perjalanan.png', 80, 5, 100, 70);
-}elseif($perjalanan['status']== 2){
+} elseif ($perjalanan['status'] == 2) {
     $pdf->Image('assets/img/watermark/sedang-perjalanan.png', 40, 5, 100, 70);
     $pdf->Image('assets/img/watermark/sedang-perjalanan.png', 80, 5, 100, 70);
 }
@@ -105,6 +105,9 @@ $pdf->Cell(45, -7, '', 0, 0, 'C', 0);
 
 $pdf->Ln(1);
 $reservasi = $this->db->get_where('reservasi', ['id' => $perjalanan['reservasi_id']])->row_array();
+$atasan1 = $this->db->get_where('karyawan', ['inisial' => substr($reservasi['atasan1'], -3)])->row_array();
+$atasan2 = $this->db->get_where('karyawan', ['inisial' => substr($reservasi['atasan2'], -3)])->row_array();
+$ga_admin = $this->db->get_where('karyawan', ['inisial' => $perjalanan['penyelesaian_by']])->row_array();
 
 $pdf->Cell(17, 5, 'Keterangan', 0, 0);
 $pdf->Cell(115, 5, ': ' . $perjalanan['catatan_ga'] . ' ' . $perjalanan['catatan_security'], 0, 0);
@@ -123,10 +126,10 @@ $pdf->setFillColor(255, 255, 255);
 $pdf->SetFont('Arial', '', 6);
 $pdf->Cell(7, 4, 'NO', 1, 0, 'C', 1);
 $pdf->Cell(41, 4, 'Nama', 1, 0, 'C', 1);
+$pdf->Cell(21, 4, 'Insentif Pagi', 1, 0, 'C', 1);
 $pdf->Cell(21, 4, 'Makan Siang', 1, 0, 'C', 1);
 $pdf->Cell(21, 4, 'Makan Malam', 1, 0, 'C', 1);
-$pdf->Cell(21, 4, 'Insentif Subuh', 1, 0, 'C', 1);
-$pdf->Cell(21, 4, 'Biaya Lain', 1, 0, 'C', 1);
+$pdf->Cell(21, 4, 'Biaya Lain*', 1, 0, 'C', 1);
 $pdf->Cell(37, 4, 'TOTAL', 1, 1, 'C', 1);
 
 $pdf->SetFont('Arial', 'B', 7);
@@ -135,32 +138,48 @@ $peserta = $this->db->get_where('perjalanan_anggota', ['perjalanan_id' => $perja
 $no = 1;
 foreach ($peserta as $p) :
 
-$pdf->Cell(7, 4, $no, 1, 0, 'C', 1);
-$pdf->Cell(41, 4, $p['karyawan_nama'], 1, 0, 'C', 1);
-$pdf->Cell(21, 4, $perjalanan['um3'], 1, 0, 'C', 1);
-$pdf->Cell(21, 4, $perjalanan['um4'], 1, 0, 'C', 1);
-$pdf->Cell(21, 4, $perjalanan['um1'], 1, 0, 'C', 1);
-$pdf->Cell(21, 4, '', 1, 0, 'C', 1);
-$pdf->Cell(37, 4, '', 1, 1, 'C', 1);
+    $pdf->Cell(7, 4, $no, 1, 0, 'C', 1);
+    $pdf->Cell(41, 4, $p['karyawan_nama'], 1, 0, 'C', 1);
+    if ($perjalanan['um1'] == 'YA') {
+        $pdf->Cell(21, 4, number_format($p['insentif_pagi'], 0, ',', '.'), 1, 0, 'C', 1);
+    } else {
+        $pdf->Cell(21, 4, '-', 1, 0, 'C', 1);
+    }
+    if ($perjalanan['um3'] == 'YA') {
+        $pdf->Cell(21, 4, number_format($p['um_siang'], 0, ',', '.'), 1, 0, 'C', 1);
+    } else {
+        $pdf->Cell(21, 4, '-', 1, 0, 'C', 1);
+    }
+    if ($perjalanan['um4'] == 'YA') {
+        $pdf->Cell(21, 4, number_format($p['um_malam'], 0, ',', '.'), 1, 0, 'C', 1);
+    } else {
+        $pdf->Cell(21, 4, '-', 1, 0, 'C', 1);
+    }
+    $pdf->Cell(21, 4, '', 1, 0, 'C', 1);
+    $pdf->Cell(37, 4, number_format($p['total'], 0, ',', '.'), 1, 1, 'C', 1);
 
-$no = $no + 1;
+    $no = $no + 1;
 
 endforeach;
 
 $pdf->Ln(3);
 $pdf->SetFont('Arial', '', 6);
-$pdf->Cell(33, 4, 'Taksi*', 1, 0, 'C', 1);
-$pdf->Cell(33, 4, 'BBM*', 1, 0, 'C', 1);
-$pdf->Cell(33, 4, 'Tol*', 1, 0, 'C', 1);
-$pdf->Cell(33, 4, 'Parkir*', 1, 0, 'C', 1);
+$pdf->Cell(33, 4, 'Taksi/Pribadi (Per Km)', 1, 0, 'C', 1);
+$pdf->Cell(33, 4, 'BBM', 1, 0, 'C', 1);
+$pdf->Cell(33, 4, 'Tol', 1, 0, 'C', 1);
+$pdf->Cell(33, 4, 'Parkir', 1, 0, 'C', 1);
 $pdf->Cell(37, 4, 'TOTAL', 1, 1, 'C', 1);
 
 $pdf->SetFont('Arial', 'B', 6);
-$pdf->Cell(33, 5, '', 1, 0, 'C', 1);
-$pdf->Cell(33, 5, '', 1, 0, 'C', 1);
-$pdf->Cell(33, 5, '', 1, 0, 'C', 1);
-$pdf->Cell(33, 5, '', 1, 0, 'C', 1);
-$pdf->Cell(37, 5, '', 1, 1, 'C', 1);
+$pdf->Cell(33, 5, number_format($perjalanan['taksi'], 0, ',', '.'), 1, 0, 'C', 1);
+$pdf->Cell(33, 5, number_format($perjalanan['bbm'], 0, ',', '.'), 1, 0, 'C', 1);
+$pdf->Cell(33, 5, number_format($perjalanan['tol'], 0, ',', '.'), 1, 0, 'C', 1);
+$pdf->Cell(33, 5, number_format($perjalanan['parkir'], 0, ',', '.'), 1, 0, 'C', 1);
+$pdf->Cell(37, 5, number_format($perjalanan['taksi'] + $perjalanan['bbm'] + $perjalanan['tol'] + $perjalanan['parkir'], 0, ',', '.'), 1, 1, 'C', 1);
+//Resume Biaya
+$pdf->SetFont('Arial', 'B', 8);
+$pdf->Cell(132, 5, 'TOTAL', 1, 0, 'C', 1);
+$pdf->Cell(37, 5, number_format($perjalanan['total'], 0, ',', '.'), 1, 1, 'C', 1);
 
 $pdf->Ln(3);
 $pdf->setTextColor(0, 0, 0);
@@ -177,44 +196,61 @@ $pdf->Cell(42, 13, '', 1, 0, 'C', 1);
 $pdf->Cell(42, 13, '', 1, 1, 'C', 1);
 
 $pdf->Cell(43, 4, 'Atasan', 1, 0, 'C', 1);
-$pdf->Cell(42, 4, 'KA.Dept', 1, 0, 'C', 1);
+$pdf->Cell(42, 4, 'KA.Dept/Div', 1, 0, 'C', 1);
 $pdf->Cell(42, 4, 'GA', 1, 0, 'C', 1);
 $pdf->Cell(42, 4, 'Pemohon', 1, 1, 'C', 1);
 
 $pdf->Ln(-16);
 $pdf->SetFont('arial-monospaced', '', 5);
-$pdf->Cell(77, 3, 'Ini adalah form digital', 0,'C', 0);
-$pdf->Cell(6.5, 8, 'Tidak memerlukan tanda tangan basah', 0,'C', 0);
+$pdf->Cell(35, 3, 'Ini adalah form digital', 0, 'C', 0);
+$pdf->Cell(6.5, 8, 'Tidak memerlukan tanda tangan basah', 0, 'C', 0);
 
-$pdf->Cell(80, 3, 'Ini adalah form digital', 0,'C', 0);
-$pdf->Cell(4, 8, 'Tidak memerlukan tanda tangan basah', 0,'C', 0);
+$pdf->Cell(36, 3, 'Ini adalah form digital', 0, 'C', 0);
+$pdf->Cell(6.5, 8, 'Tidak memerlukan tanda tangan basah', 0, 'C', 0);
+if (empty($perjalanan['penyelesaian_by'])) {
+    $pdf->Cell(80, 3, 'Ini adalah form digital', 0, 'C', 0);
+    $pdf->Cell(4, 8, 'Tidak memerlukan tanda tangan basah', 0, 'C', 0);
+} else {
+    $pdf->Cell(37, 3, 'Ini adalah form digital', 0, 'C', 0);
+    $pdf->Cell(4, 8, 'Tidak memerlukan tanda tangan basah', 0, 'C', 0);
+    $pdf->Cell(38, 3, 'Ini adalah form digital', 0, 'C', 0);
+    $pdf->Cell(4, 8, 'Tidak memerlukan tanda tangan basah', 0, 'C', 0);
+}
 
 
 $pdf->Ln(5);
 $pdf->SetFont('Arial', 'B', 6);
-$pdf->Cell(55, 5,'',0,0,'C',0);
-$pdf->Cell(20, 5, $perjalanan['ka_dept'], 0, 0, 'C',0);
-$pdf->Cell(65, 5,'',0,0,'C',0);
-$pdf->Cell(20, 5, $perjalanan['nama'], 0, 1, 'C');
+$pdf->Cell(43, 5, $atasan1['nama'], 0, 0, 'C', 0);
+$pdf->Cell(42, 5, $atasan2['nama'], 0, 0, 'C', 0);
+if (empty($perjalanan['penyelesaian_by'])) {
+    $pdf->Cell(65, 5, '', 0, 0, 'C', 0);
+    $pdf->Cell(1, 5, $perjalanan['nama'], 0, 1, 'C');
+} else {
+    $pdf->Cell(43, 5, $ga_admin['nama'], 0, 0, 'C', 0);
+    $pdf->Cell(42, 5, $perjalanan['nama'], 0, 1, 'C');
+}
 
 $pdf->Ln(-2);
 $pdf->SetFont('Arial', '', 6);
-$pdf->Cell(55, 5, '', 110, 0);
-$pdf->Cell(20, 5, 'Disetujui pada ' . date('d/m/Y H:i', strtotime($reservasi['tgl_atasan2'])), 110, 0, 'C');
-$pdf->Cell(65, 5,'',0,0,'C',0);
-$pdf->Cell(20, 5, 'Dibuat pada ' . date('d/m/Y H:i', strtotime($reservasi['tglreservasi'])), 110, 0, 'C');
+$pdf->Cell(45, 5, 'Disetujui pada ' . date('d/m/Y H:i', strtotime($reservasi['tgl_atasan1'])), 110, 0, 'C');
+$pdf->Cell(40, 5, 'Disetujui pada ' . date('d/m/Y H:i', strtotime($reservasi['tgl_atasan2'])), 110, 0, 'C');
+if (empty($perjalanan['penyelesaian_by'])) {
+    $pdf->Cell(65, 5, '', 0, 0, 'C', 0);
+    $pdf->Cell(1, 5, 'Dibuat pada ' . date('d/m/Y H:i', strtotime($reservasi['tglreservasi'])), 110, 0, 'C');
+} else {
+    $pdf->Cell(41, 5, 'Diperiksa pada ' . date('d/m/Y H:i', strtotime($perjalanan['penyelesaian_at'])), 110, 0, 'C');
+    $pdf->Cell(45, 5, 'Dibuat pada ' . date('d/m/Y H:i', strtotime($reservasi['tglreservasi'])), 110, 0, 'C');
+}
 
 $pdf->Ln(8.5);
 $pdf->SetFont('Arial', '', 6);
 $pdf->Cell(32, 2, '(*) Di isi Manual', 0, 1);
 $pdf->SetFont('Arial', '', 4);
+$pdf->SetTextColor(255, 0, 0);
 $pdf->Ln(1);
 $pdf->Cell(32, 2, 'Dengan melakukan perjalanan ini, saya setuju untuk:', 0, 1);
-$pdf->Cell(32, 2, '1. Mengemudi dengan aman dan selalu menggunakan sabuk keselamatan', 0, 1);
-$pdf->Cell(32, 2, '2. Tidak menaruh barang-barang di dashboard karena dapat mengganggu fungsi airbag', 0, 1);
-$pdf->Cell(32, 2, '3. Menjaga kebersihan kendaraan dan tidak meninggalkan sampah dan barang-barang lainnya', 0, 1);
-$pdf->Cell(32, 2, '4. Menghargai pengguna berikutnya dengan mengembalikan kendaraan dalam kondisi bersih dan rapih', 0, 1);
-$pdf->Cell(32, 2, '5. Mematuhi peraturan perusahaan yang berlaku', 0, 1);
+$pdf->Cell(32, 2, '1. Mengemudi dengan aman dan selalu menggunakan sabuk keselamatan | 2. Tidak menaruh barang-barang di dashboard karena dapat mengganggu fungsi airbag | 3. Menjaga kebersihan kendaraan dan tidak meninggalkan sampah dan barang-barang lainnya', 0, 1);
+$pdf->Cell(32, 2, '4. Menghargai pengguna berikutnya dengan mengembalikan kendaraan dalam kondisi bersih dan rapih | 5. Mematuhi peraturan perusahaan yang berlaku', 0, 1);
 
 //$pdf->Output();
 //OUTPUT SET FILENAME DOWNLOAD
