a611df80183f1238ff9a9eefca21be2ab0a39b14
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-07-06 12:06:39 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -16,6 +16,63 @@ class Dashboard extends CI_Controller
     public function index()
     {
         date_default_timezone_set('asia/jakarta');
+        $n = time();
+        $m = strtotime(date('Y-m-d 16:00:00'));
+        if ($n > $m){
+            $id = 'GA'.date('ymd');
+            $cekn = $this->db->get_where('notifikasi', ['id' =>  $id])->row_array();
+            if (empty($cekn)){
+                $today = date('d');
+                $bulan = date('m');
+                $tahun = date('Y');
+                $this->db->where('year(tglmulai)',$tahun);
+                $this->db->where('month(tglmulai)',$bulan);
+                $this->db->where('day(tglmulai)',$today);
+                $this->db->where('lokasi !=','WTQ');
+                $this->db->where('status >', '2');
+                $lembur_cus = $this->db->get('lembur');
+
+                $this->db->where('year(tglmulai)',$tahun);
+                $this->db->where('month(tglmulai)',$bulan);
+                $this->db->where('day(tglmulai)',$today);
+                $this->db->where('lokasi','WTQ');
+                $this->db->where('status >', '2');
+                $lembur_wtq = $this->db->get('lembur');
+
+                $data = array(
+                    'id' => $id,
+                    'notifikasi' => 1,
+                    'tanggal' => date('Y-m-d H:i:s')
+                );
+                $this->db->insert('notifikasi', $data);
+
+                $this->db->where('sect_id', '214');
+                $ga_admin = $this->db->get('karyawan_admin')->row_array();
+             
+                $client = new \GuzzleHttp\Client();
+                $response = $client->post(
+                    'https://region01.krmpesan.com/api/v2/message/send-text',
+                    [
+                        'headers' => [
+                            'Content-Type' => 'application/json',
+                            'Accept' => 'application/json',
+                            'Authorization' => 'Bearer zrIchFm6ewt2f18SbXRcNzSVXJrQBEsD1zrbjtxuZCyi6JfOAcRIQkrL6wEmChqVWwl0De3yxAhJAuKS',
+                        ],
+                        'json' => [
+                            'phone' => $ga_admin['phone'],
+                            'message' => "*INFORMASI LEMBUR HARI INI*" . 
+                            "\r\n \r\nTanggal : *" . date('d M Y') . "*" .
+                            "\r\nLembur di WINTEQ : *" . $lembur_wtq->num_rows() . "*" .
+                            "\r\nLembur di Customer : *" . $lembur_cus->num_rows() . "*" .
+                            "\r\n \r\nMohon segera konfirmasi untuk konsumsi.".
+                            "\r\n \r\nUntuk informasi lebih lengkap silahkan buka portal aplikasi di link berikut https://raisa.winteq-astra.com"
+                        ],
+                    ]
+                );
+                $body = $response->getBody();
+            }
+        }
+
         $notifikasi = $this->db->get_where('layanan_notifikasi', ['id' => '1'])->row_array();
         //Auto batalkan perjalanan
         $queryPerjalanan = "SELECT *
