484a0a9480f3abf916a55ba2bfa5165b95f41c8b
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-07-24 15:08:11 +0700

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -4,6 +4,14 @@
     <div class="row">
       <div class="col-md-12">
         <div class="alert alert-default" role="alert">
+          <strong>LINK WEBINAR</strong>
+          </br>https://teams.microsoft.com/dl/launcher/launcher.html?type=meetup-join&deeplinkId=2f81ece6-58cf-4b19-b57b-1b94bbcd5be2&directDl=true&msLaunch=true&enableMobilePage=true&url=%2F_%23%2Fl%2Fmeetup-join%2F19%253ameeting_MzI1ZWIzYWQtMDgyNi00ZWRiLWIxZGYtYTc5OTcxYmIxNjA0%2540thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522f5332409-fcd5-4d5d-aaf7-b5189010d0d0%2522%252c%2522Oid%2522%253a%25223509aa3d-d95b-4588-824e-a17f86b4063e%2522%252c%2522IsBroadcastMeeting%2522%253atrue%257d%26anon%3Dtrue&suppressPrompt=true
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="alert alert-default" role="alert">
           <strong>INFO : LAYANAN e-Wallet OVO DIBERHENTIKAN</strong>
           </br>
           </br>Hai <?= $karyawan['nama']; ?>,
