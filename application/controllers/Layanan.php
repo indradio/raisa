5bcadb10c0dbc980ca50a58d985955fc3b5f1e98
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-07-24 15:11:31 +0700

    Update Layanan.php

--- application/controllers/Layanan.php
+++ application/controllers/Layanan.php
@@ -194,7 +194,7 @@ class Layanan extends CI_Controller
                              "\r\n \r\nSemangat Pagi, Hai *" . $k['nama'] . "*" .
                              "\r\n \r\nYuk gabung di webinar, sebentar lagi akan segera mulai." .
                              "\r\n \r\nKlik link dibahaw ini ya" .
-                             "\r\n \r\n*https://teams.microsoft.com/dl/launcher/launcher.html?type=meetup-join&deeplinkId=2f81ece6-58cf-4b19-b57b-1b94bbcd5be2&directDl=true&msLaunch=true&enableMobilePage=true&url=%2F_%23%2Fl%2Fmeetup-join%2F19%253ameeting_MzI1ZWIzYWQtMDgyNi00ZWRiLWIxZGYtYTc5OTcxYmIxNjA0%2540thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522f5332409-fcd5-4d5d-aaf7-b5189010d0d0%2522%252c%2522Oid%2522%253a%25223509aa3d-d95b-4588-824e-a17f86b4063e%2522%252c%2522IsBroadcastMeeting%2522%253atrue%257d%26anon%3Dtrue&suppressPrompt=true*" .
+                             "\r\n \r\n*https://raisa.winteq-astra.com/meeting/webinar*".
                              "\r\n \r\n \r\n*INGAT! Buat kamu yang nobar, Tetap menjaga jarak, Gunakan masker dan menjalankan protokol kesehatan selama webinar berlangsung.*"
                          ],
                      ]
@@ -225,7 +225,7 @@ class Layanan extends CI_Controller
                             "\r\n \r\nSemangat Pagi, Hai *" . $k['nama'] . "*" .
                             "\r\n \r\nYuk gabung di webinar, sebentar lagi akan segera mulai." .
                             "\r\n \r\nKlik link dibahaw ini ya" .
-                            "\r\n \r\n*https://teams.microsoft.com/dl/launcher/launcher.html?type=meetup-join&deeplinkId=2f81ece6-58cf-4b19-b57b-1b94bbcd5be2&directDl=true&msLaunch=true&enableMobilePage=true&url=%2F_%23%2Fl%2Fmeetup-join%2F19%253ameeting_MzI1ZWIzYWQtMDgyNi00ZWRiLWIxZGYtYTc5OTcxYmIxNjA0%2540thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522f5332409-fcd5-4d5d-aaf7-b5189010d0d0%2522%252c%2522Oid%2522%253a%25223509aa3d-d95b-4588-824e-a17f86b4063e%2522%252c%2522IsBroadcastMeeting%2522%253atrue%257d%26anon%3Dtrue&suppressPrompt=true*" .
+                            "\r\n \r\n*https://raisa.winteq-astra.com/meeting/webinar*".
                             "\r\n \r\n \r\n*INGAT! Buat kamu yang nobar, Tetap menjaga jarak, Gunakan masker dan menjalankan protokol kesehatan selama webinar berlangsung.*"
                         ],
                     ]
@@ -256,7 +256,7 @@ class Layanan extends CI_Controller
                             "\r\n \r\nSemangat Pagi, Hai *" . $k['nama'] . "*" .
                             "\r\n \r\nYuk gabung di webinar, sebentar lagi akan segera mulai." .
                             "\r\n \r\nKlik link dibahaw ini ya" .
-                            "\r\n \r\n*https://teams.microsoft.com/dl/launcher/launcher.html?type=meetup-join&deeplinkId=2f81ece6-58cf-4b19-b57b-1b94bbcd5be2&directDl=true&msLaunch=true&enableMobilePage=true&url=%2F_%23%2Fl%2Fmeetup-join%2F19%253ameeting_MzI1ZWIzYWQtMDgyNi00ZWRiLWIxZGYtYTc5OTcxYmIxNjA0%2540thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%2522f5332409-fcd5-4d5d-aaf7-b5189010d0d0%2522%252c%2522Oid%2522%253a%25223509aa3d-d95b-4588-824e-a17f86b4063e%2522%252c%2522IsBroadcastMeeting%2522%253atrue%257d%26anon%3Dtrue&suppressPrompt=true*" .
+                            "\r\n \r\n*https://raisa.winteq-astra.com/meeting/webinar*".
                             "\r\n \r\n \r\n*INGAT! Buat kamu yang nobar, Tetap menjaga jarak, Gunakan masker dan menjalankan protokol kesehatan selama webinar berlangsung.*"
                         ],
                     ]
