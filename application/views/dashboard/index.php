0564d231393f824822d3b21fda5a50c3dedda76c
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-07-29 09:46:55 +0700

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -17,64 +17,6 @@
       </div>
     </div>
     <!-- End Banner -->
-    <?php if (empty($karyawan['ewallet_1'])){ ?>
-    <div class="row">
-      <div class="col-md-12">
-        <form id="timePrecense" class="form" method="post" action="<?= base_url('profil/submit/e-wallet'); ?>">
-          <div class="card ">
-            <div class="card-header card-header-danger card-header-icon">
-              <div class="card-icon">
-                <i class="material-icons">account_balance_wallet</i>
-              </div>
-              <h4 class="card-title">KAMU BELUM MEMDAFTARKAN e-WALLET NIH, DAFTARKAN e-WALLET SEKARANG KUY!</h4>
-            </div>
-            <div class="card-body ">
-              <div class="progress" style="width: 100%">
-                <div class="progress-bar progress-bar-danger" role="progressbar" style="width: 100%" aria-valuenow="1" aria-valuemin="0" aria-valuemax="1"></div>
-              </div>
-              <div class="form-group">
-                <label for="a1" class="bmd-label-floating">Utama* <small>( Wajib )</small></label>
-                <select class="form-control selectpicker" data-style="btn btn-link" id="utama" name="utama" title="Silahkan Pilih" data-size="3" data-live-search="false" required>
-                  <option value="OVO">OVO</option>
-                  <option value="DANA">DANA</option>
-                  <option value="GO-PAY">GO-PAY</option>
-                </select>
-                <div class="form-group has-default">
-                <input type="text" class="form-control" name="utama_rek" placeholder="Nomor Rek/HP e-Wallet" required="true">
-              </div>
-              </div>
-              <div class="form-group">
-                <label for="a2" class="bmd-label-floating">Cadangan <small>( Opsional )</small></label>
-                <select class="form-control selectpicker" data-style="btn btn-link" id="cadangan" name="cadangan" title="Silahkan Pilih" data-size="3" data-live-search="false">
-                  <option value="OVO">OVO</option>
-                  <option value="DANA">DANA</option>
-                  <option value="GO-PAY">GO-PAY</option>
-                </select>
-                <div class="form-group has-default">
-                <input type="text" class="form-control" name="cadangan_rek" placeholder="Nomor Rek/HP e-Wallet">
-              </div>
-              </div>
-              <div class="form-check mr-auto">
-                <label class="form-check-label text-dark">
-                  <input class="form-check-input" type="checkbox" id="check" value="" required>
-                  <span class="form-check-sign">
-                    <span class="check"></span>
-                  </span>
-                  <i>Dengan ini saya menyatakan telah memberikan informasi yang benar, kesalahan nomor rekening adalah tanggung jawab saya.</i>
-                </label>
-              </div>
-              <div class="category form-category">
-              </div>
-            </div>
-            <div class="card-footer ml-auto">
-             <button type="submit" id="submit" class="btn btn-success">SUBMIT</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-    <!-- end row -->
-    <?php } ?>
     <div class="row">
       <?php
       $queryLayInfo = "SELECT COUNT(*)
