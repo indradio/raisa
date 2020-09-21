24c5628fac30fa9c753e2028282e7976cfd43069
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2020-09-21 07:38:58 +0700

    Update Perjalanandl.php

--- application/controllers/Perjalanandl.php
+++ application/controllers/Perjalanandl.php
@@ -737,7 +737,7 @@ class Perjalanandl extends CI_Controller
                         'message' => "*WAKTU KEBERANGKATAN PERJALANAN DINAS INI MUNDUR*". 
                         "\r\n \r\n*#" . $perjalanan['id'] . "*" .
                         "\r\nTujuan : *" .$perjalanan['tujuan']. "*" .
-                        "\r\nPeserta: *" . $perjalanan['peserta'] . "*" .
+                        "\r\nPeserta: *" . $perjalanan['anggota'] . "*" .
                         "\r\nWaktu Keberangkatan Menjadi: *" . date('H:i:s', strtotime('+1 hour', strtotime($perjalanan['jamberangkat']))) . "*"
                     ],
                 ]
@@ -761,7 +761,7 @@ class Perjalanandl extends CI_Controller
                         'message' => "*WAKTU KEBERANGKATAN PERJALANAN DINAS INI MUNDUR*". 
                         "\r\n \r\n*#" . $perjalanan['id'] . "*" .
                         "\r\nTujuan : *" .$perjalanan['tujuan']. "*" .
-                        "\r\nPeserta: *" . $perjalanan['peserta'] . "*" .
+                        "\r\nPeserta: *" . $perjalanan['anggota'] . "*" .
                         "\r\nWaktu Keberangkatan Menjadi: *" . date('H:i:s', strtotime('+1 hour', strtotime($perjalanan['jamberangkat']))) . "*"
                     ],
                 ]
