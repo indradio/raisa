a2942887d4ea0177edff6cd1433facd98b41a835
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2021-01-18 08:21:44 +0700

    Update Perjalanandl.php

--- application/controllers/Perjalanandl.php
+++ application/controllers/Perjalanandl.php
@@ -2035,7 +2035,6 @@ class Perjalanandl extends CI_Controller
             $data['sidesubmenu'] = 'Permintaan Kasbon';
             $data['karyawan'] = $this->db->get_where('karyawan', ['npk' => $this->session->userdata('npk')])->row_array();
             $data['perjalanan'] = $this->db->order_by('id', 'DESC');
-            $data['perjalanan'] = $this->db->limit('20');
             $data['perjalanan'] = $this->db->where('kasbon_out >', 0);
             $data['perjalanan'] = $this->db->or_where('kasbon >', 0);
             $data['perjalanan'] = $this->db->get('perjalanan')->result_array();
