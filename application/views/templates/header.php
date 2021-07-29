610c0968f91eda84d7bb99422b6c71705dfe762c
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2021-07-29 18:57:18 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,47 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php date_default_timezone_set('asia/jakarta'); ?>
+
 <head>
+
     <meta charset="utf-8" />
-    <!-- <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url(); ?>assets/img/apple-icon.png"> -->
     <link rel="icon" type="image/png" href="<?= base_url(); ?>assets/img/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
     <title>
         RAISA | Report And Integrated System of Administrations
     </title>
+
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
     <!-- CSS Files -->
     <link href="<?= base_url(); ?>assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+
     <!-- CSS PhotoSwipe -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/photoswipe.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/default-skin/default-skin.min.css" rel="stylesheet" />
+
     <!-- Plugin for tinymce  -->
     <script src="<?= base_url(); ?>assets/js/plugins/tinymce.min.js"></script>
     <script src="<?= base_url(); ?>assets/js/plugins/jquery.mask.js"></script>
     <script src="<?= base_url(); ?>assets/js/plugins/jquery.mask.min.js"></script>
-    <!-- <script src="<?= base_url(); ?>assets/js/plugins/OneSignalSDKUpdaterWorker.js"></script> -->
-    <!-- <script src="<?= base_url(); ?>assets/js/plugins/OneSignalSDKWorker.js"></script> -->
+
     <!-- Plugin for jquery  -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    
     <!-- Plugin for PhotoSwipe  -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/photoswipe.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.1/photoswipe-ui-default.min.js"></script>
-    <!-- <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
-    <script>
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function() {
-        OneSignal.init({
-            appId: "39c59297-a627-4ae7-ac1d-4fd1e0b75c9f",
-            safari_web_id: "web.onesignal.auto.49d2239d-a04e-422a-89e0-14dbda97fb4d",
-            notifyButton: {
-                enable: true,
-            },
-        });
-    });
-    </script> -->
+   
 </head>
