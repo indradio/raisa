a518b952d0c89e1d7f4a54adcc054e0e25d61a9c
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2021-09-01 10:37:55 +0700

    Update kasbon_fa_daftar.php

--- application/views/perjalanandl/kasbon_fa_daftar.php
+++ application/views/perjalanandl/kasbon_fa_daftar.php
@@ -15,7 +15,7 @@
                             <!--        Here you can write extra buttons/actions for the toolbar              -->
                         </div>
                         <div class="material-datatables">
-                            <table id="dtdesc" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                            <table id="dtkasbon" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
                                 <thead>
                                     <tr>
                                         <th>No Perjalanan</th>
@@ -288,5 +288,21 @@
             modal.find('.modal-body input[name="kasbon_in"]').val(kasbon_in)
             modal.find('.modal-body input[name="kasbon"]').val(kasbon)
         })
+
+        $('#dtkasbon').DataTable({
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+            [10, 25, 50, -1],
+            [10, 25, 50, "All"]
+        ],
+        order: [
+            [11, 'desc']
+        ],
+        scrollX: true,
+        language: {
+            search: "_INPUT_",
+            searchPlaceholder: "Search records",
+        }
+    });
     });
 </script>
