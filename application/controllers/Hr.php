c03b66dca798f274e23955880362d6465dfc0bed
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2021-10-13 13:01:29 +0700

    up up

--- application/controllers/Hr.php
+++ application/controllers/Hr.php
@@ -10,14 +10,6 @@ class Hr extends CI_Controller
         date_default_timezone_set('asia/jakarta');
         
         $this->load->model("Karyawan_model");
-
-        $this->db->where('npk', $this->session->userdata('npk'));
-        $this->db->where('date', date('Y-m-d'));
-        $complete = $this->db->get('kesehatan')->row_array();
-
-        if (empty($complete)){
-            redirect('dashboard/sehat');
-        }
     }
 
     public function karyawan()
