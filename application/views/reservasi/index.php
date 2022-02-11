2a71e6dde803c7824126f228e1b8dada8b7e0add
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2022-02-11 19:09:27 +0700

    Update index.php

--- application/views/reservasi/index.php
+++ application/views/reservasi/index.php
@@ -4,7 +4,7 @@
     <div class="row">
       <div class="col-md-12">
         <div class="card">
-          <div class="card-header card-header-primary card-header-icon">
+          <div class="card-header card-header-info card-header-icon">
             <div class="card-icon">
               <i class="material-icons">assignment</i>
             </div>
@@ -13,7 +13,7 @@
           <div class="card-body">
             <div class="toolbar">
               <!--        Here you can write extra buttons/actions for the toolbar              -->
-              <a href="<?= base_url('reservasi/dl'); ?>" class="btn btn-facebook mb-2" role="button" aria-disabled="false">Reservasi Perjalanan Baru</a>
+              <a href="#" id="btn_reservasi" class="btn btn-facebook mb-2" role="button" aria-disabled="false">Reservasi Perjalanan Baru</a>
             </div>
             <div class="material-datatables">
               <table id="dtdesc" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
@@ -115,3 +115,44 @@
     </div>
   </div>
 </div>
+
+<script>
+  $(document).ready(function() {
+    $('#btn_reservasi').on('click',function(){
+    
+      var now_d = '<?= date('D') ?>';
+      let now_t = <?= strtotime(date('H:i:s')) ?>;
+      let min_t = <?= strtotime(date('07:30:00')) ?>;
+      let max_t = <?= strtotime(date('16:30:00')) ?>;
+
+      if (now_d == 'Sat' || now_d == 'Sun' || now_t < min_t || now_t > max_t) {
+
+      Swal.fire({
+        title: 'Perhatian!',
+        icon: 'warning',
+        html:
+        'Kamu mengajukan permohonan di luar jam kerja, untuk itu harap kontak langsung pimpinan-pimpinan kamu dan pihak yang berkepentingan untuk merelease permohonan kamu.</p> ' +
+        'Sudah mengajukan di RAISA tidak berarti menyerahkan tanggung jawab kamu ke RAISA, RAISA tidak bertanggung jawab atas permohonan kamu yang tidak kunjung disetujui, kamu tetap harus proaktif.',
+        showCancelButton: false,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Ya, Saya Mengerti',
+        showClass: {
+          popup: 'animate__animated animate__heartBeat'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOut'
+        }
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "<?= base_url('reservasi/dl'); ?>";
+        }
+      });
+
+      }else{
+
+        window.location.href = "<?= base_url('reservasi/dl'); ?>";
+
+      }
+    });
+  });
+</script>
