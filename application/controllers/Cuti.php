279a4b2661b42d35fc8de183b628ee083b516572
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2022-08-02 09:48:11 +0700

    Update Cuti.php

--- application/controllers/Cuti.php
+++ application/controllers/Cuti.php
@@ -710,7 +710,7 @@ class Cuti extends CI_Controller
             'tglakhir'  => $data['tglakhir']
         ];
         
-        $this->db->where('tgl1 >=', $data['tglawal']);
+        $this->db->where('tgl2 >=', $data['tglawal']);
         $this->db->where('tgl1 <=', $data['tglakhir']);
         $this->db->where('status >', 0);
         $data['cuti'] = $this->db->get_where('cuti')->result();
