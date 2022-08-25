e2428a676c36cce1ef0ca4e609ed32453d8efea2
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2022-08-25 17:58:01 +0700

    Update

--- application/views/lembur/persetujuan/realisasi.php
+++ application/views/lembur/persetujuan/realisasi.php
@@ -106,7 +106,7 @@
                             </div> 
                             <!-- Button SUBMIT -->
                             <button type="submit" id="setujui" class="btn btn-sm btn-success">SETUJUI</button>
-                            <a href="#" id="batalAktivitas" class="btn btn-sm btn-danger" role="button" aria-disabled="false" data-toggle="modal" data-target="#batalRsv" data-id="<?= $lembur['id']; ?>">BATALKAN</a>
+                            <a href="#" class="btn btn-sm btn-danger" role="button" aria-disabled="false" data-toggle="modal" data-target="#batalLembur" data-id_lembur="<?= $lembur['id']; ?>">BATALKAN</a>
                             <a href="<?= base_url('lembur/persetujuan') ?>" class="btn btn-sm btn-default" role="button">Kembali</a>
                         </div>
                     </form>   
@@ -141,6 +141,33 @@
     </div>
   </div>
 </div>
+
+<div class="modal fade" id="batalLembur" tabindex="-1" role="dialog" aria-labelledby="batalLemburTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="card card-signup card-plain">
+      <form class="form" id="formbatalLembur" method="post" action="<?= base_url('lembur/reject/realisasi'); ?>">
+          <div class="modal-body">
+              <input type="text" class="form-control" id="id_lembur_batal" name="id_lembur_batal" value="<?= $lembur['id']; ?>"/>
+              <h4 class="card-title text-center">Yakin ingin membatalkan lembur ini?</h4>
+              <div class="row">
+                      <div class="col-md-12">
+                          <div class="form-group">
+                              <label class="bmd-label-floating"><small>Alasan</small></label></br>
+                              <textarea class="form-control has-success" id="catatan" name="catatan" rows="3" required="true"></textarea>
+                          </div>
+                      </div>
+                    </div>
+          </div>
+          <div class="modal-footer justify-content-center">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">KEMBALI</button>
+              <button type="submit" class="btn btn-danger" id="btn_batal_aktivitas" >YA, BATALKAN!</button>
+          </div>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
 <!-- script ajax Kategori-->
 <script type="text/javascript">        
         $(document).ready(function(){
@@ -161,7 +188,7 @@
                         "data"  : {id:$('#id').val()}
                     },
                 columns: [
-                    { "data": "aktivitas" },
+                    { "data": "aktivitas" },wd
                     { "data": "deskripsi" },
                     { "data": "durasi" }
                 ],
@@ -284,5 +311,11 @@
                 return false;
             });
 
+            $('#batalLembur').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget) // Button that triggered the modal
+                var id_lembur = button.data('id_lembur')
+                var modal = $(this)
+                modal.find('.modal-body input[name="id_lembur_batal"]').val(id_lembur);
+            });
         });  
     </script>
