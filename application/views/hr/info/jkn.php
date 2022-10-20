5a5a00b24ff2f1bc40110fade87f8c518844b1f5
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2022-10-20 15:53:49 +0700

    update jkn

---
+++ application/views/hr/info/jkn.php
@@ -0,0 +1,61 @@
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card ">
+                    <div class="card-header ">
+                        <h4 class="card-title">Panduan Penggunaan dan FAQ - JKN
+                            <!-- <small class="description">Horizontal Tabs</small> -->
+                        </h4>
+                    </div>
+                    <div class="card-body ">
+                        <ul class="nav nav-pills nav-pills-info" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#link1" role="tablist">
+                                    Mobile JKN
+                                </a>
+                            </li>
+                            <!-- <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#link2" role="tablist">
+                                    E-Claim Medcare Via Aplikasi
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#link3" role="tablist">
+                                    Video Panduan Via Aplikasi
+                                </a>
+                            </li> -->
+                        </ul>
+                        <div class="tab-content tab-space">
+                            <div class="tab-pane active" id="link1">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="<?= base_url(); ?>assets/pdf/PANDUAN PENGGUNAAN JKN.pdf"></iframe>
+                                </div>
+                                </br>
+                                <a href="<?= base_url(); ?>assets/pdf/PANDUAN PENGGUNAAN JKN.pdf" target="_blank">Klik Disini untuk download</a>
+                            </div>
+                            <!-- <div class="tab-pane" id="link2">
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="<?= base_url(); ?>assets/pdf/E-Claim Medcare.pdf"></iframe>
+                                </div>
+                                </br>
+                                <a href="<?= base_url(); ?>assets/pdf/E-Claim Medcare.pdf" target="_blank">Klik Disini untuk download</a>
+                            </div>
+                            <div class="tab-pane" id="link3">
+                                <video width="100%" height="auto" controls>
+                                    <source src="<?= base_url(); ?>assets/video/medcare.mp4" type="video/mp4">
+                                </video>
+                                </br>
+                                <a href="<?= base_url(); ?>assets/video/medcare.mp4" target="_blank">Klik Disini untuk download</a>
+                            </div> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end col-md-12 -->
+        </div>
+        <!-- end row -->
+    </div>
+    <!-- end container-fluid-->
+</div>
+<!-- end content-->
