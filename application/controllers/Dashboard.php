26c10c19fc2988b841aed9eaf697f674d36596d1
Indra Dio Aji Prayoga <indrada.prayoga@gmail.com>
2023-02-09 20:10:55 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -16,6 +16,7 @@ class Dashboard extends CI_Controller
         $this->update_perjalanan();
         $this->update_lembur();
         $this->update_cuti();
+        $this->update_presensi();
     }
 
     public function update_perjalanan()
@@ -379,6 +380,62 @@ class Dashboard extends CI_Controller
                 $body = $response->getBody();
             endforeach;
 
+    }
+
+    public function update_presensi()
+    {
+        if (date('H:i:s') > date('07:35:00') AND date('H:i:s') < date('08:00:00'))
+        {
+            $id = 'IN'.date('ymd');
+            $notif = $this->db->get_where('notifikasi', ['id' =>  $id])->row();
+
+            if (empty($notif))
+            {
+                $data = array(
+                    'id' => $id,
+                    'notifikasi' => 1,
+                    'tanggal' => date('Y-m-d H:i:s')
+                );
+                $this->db->insert('notifikasi', $data);
+
+
+                $this->db->where('posisi_id >','4');
+                $this->db->where('is_active','1');;
+                $this->db->where('status','1');
+                $queryUsers = $this->db->get('karyawan')->result();
+
+                foreach ($queryUsers as $user) :
+
+                    $this->db->where('date', date('Y-m-d'));
+                    $this->db->where('npk',$user->npk);
+                    $presensi = $this->db->get('presensi')->row();
+                    if (empty($presensi))
+                    {
+                            $client = new \GuzzleHttp\Client();
+                            $response = $client->post(
+                                'https://region01.krmpesan.com/api/v2/message/send-text',
+                                [
+                                    'headers' => [
+                                        'Content-Type' => 'application/json',
+                                        'Accept' => 'application/json',
+                                        'Authorization' => 'Bearer zrIchFm6ewt2f18SbXRcNzSVXJrQBEsD1zrbjtxuZCyi6JfOAcRIQkrL6wEmChqVWwl0De3yxAhJAuKS',
+                                    ],
+                                    'json' => [
+                                        'phone' => $user->phone,
+                                        'message' => "*[NOTIF] ANDA BELUM ABSEN*" .
+                                        "\r\n \r\nSemangat pagi *" . $user->nama . "*" .
+                                        "\r\n \r\n*ANDA BELUM MELAKUKAN ABSENSI HARI INI*" .
+                                        "\r\nSilahkan segera absen melalui FACE ID ataupun RAISA".
+                                        "\r\n \r\nAbaikan jika kamu sedang cuti/off/ijin lainnya"
+                                    ],
+                                ]
+                            );
+                            $body = $response->getBody();
+                    }
+
+                endforeach;
+            }
+        }
     }
 
     public function index()
