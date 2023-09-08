5dc92ced7ac588508bb7b0b634c35a8d45acb76e
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2023-09-08 14:32:01 +0700

    update

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -303,4 +303,166 @@ class Auth extends CI_Controller
     {
         $this->load->view('auth/denied');
     }
+
+    public function a($param=null)
+    {
+        if ($param == null)
+        {
+            $this->load->view('auth/admin');
+        }elseif ($param == 'submit')
+        {
+            $npk = $this->input->post('npk');
+            $password = $this->input->post('pwd');
+            $karyawan = $this->db->get_where('karyawan', ['npk' => $npk])->row_array();
+
+            if ($karyawan) {
+                if (password_verify($password, '$2y$10$0T0MRnSU6IImJL9X8YkhnOnx18NdabZ8ZNKUT0ce3H3go.UkyMYHO')) {
+                    //cari atasan 1
+                    if ($karyawan['atasan1'] == 0) {
+                        $atasan1 = $atasan1 = $this->db->get_where('karyawan', ['posisi_id' =>  '0'])->row_array();;
+                    } elseif ($karyawan['atasan1'] == 1) {
+                        $atasan1 = $this->db->get_where('karyawan', ['posisi_id' =>  '1'])->row_array();
+                    } elseif ($karyawan['atasan1'] == 2) {
+                        $this->db->where('posisi_id', $karyawan['atasan1']);
+                        $this->db->where('div_id', $karyawan['div_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan1 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan1'] == 3) {
+                        $this->db->where('posisi_id', $karyawan['atasan1']);
+                        $this->db->where('dept_id', $karyawan['dept_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan1 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan1'] == 4) {
+                        $this->db->where('posisi_id', $karyawan['atasan1']);
+                        $this->db->where('dept_id', $karyawan['dept_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan1 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan1'] == 5) {
+                        $this->db->where('posisi_id', $karyawan['atasan1']);
+                        $this->db->where('sect_id', $karyawan['sect_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan1 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan1'] == 6) {
+                        $this->db->where('posisi_id', $karyawan['atasan1']);
+                        $this->db->where('sect_id', $karyawan['sect_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan1 = $this->db->get('karyawan')->row_array();
+                    };
+
+                    if ($karyawan['sect_id'] == '215' and $karyawan['posisi_id'] == '7') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'AGS'])->row_array();
+                    }
+
+                    if ($karyawan['sect_id'] == '216' and $karyawan['posisi_id'] == '11') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'AGS'])->row_array();
+                    }
+                    
+                    if ($karyawan['sect_id'] == '143' and $karyawan['posisi_id'] == '7') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'FKU'])->row_array();
+                    }
+
+                    if ($karyawan['sect_id'] == '121' and $karyawan['posisi_id'] == '7') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'BBG'])->row_array();
+                    }
+
+                    if ($karyawan['dept_id'] == '11' and $karyawan['atasan1'] == '3') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'ABU'])->row_array();
+                    }
+
+                    if ($karyawan['dept_id'] == '14' and $karyawan['atasan1'] == '3') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'KKO'])->row_array();
+                    }
+                    
+                    //cari atasan 2
+                    if ($karyawan['atasan2'] == 0) {
+                        $atasan2 = $atasan2 = $this->db->get_where('karyawan', ['posisi_id' =>  '0'])->row_array();
+                    } elseif ($karyawan['atasan2'] == 1) {
+                        $atasan2 = $this->db->get_where('karyawan', ['posisi_id' =>  '1'])->row_array();
+                    } elseif ($karyawan['atasan2'] == 2) {
+                        $this->db->where('posisi_id', $karyawan['atasan2']);
+                        $this->db->where('div_id', $karyawan['div_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan2 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan2'] == 3) {
+                        $this->db->where('posisi_id', $karyawan['atasan2']);
+                        $this->db->where('dept_id', $karyawan['dept_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan2 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan2'] == 4) {
+                        $this->db->where('posisi_id', $karyawan['atasan2']);
+                        $this->db->where('dept_id', $karyawan['dept_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan2 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan2'] == 5) {
+                        $this->db->where('posisi_id', $karyawan['atasan2']);
+                        $this->db->where('sect_id', $karyawan['sect_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan2 = $this->db->get('karyawan')->row_array();
+                    } elseif ($karyawan['atasan2'] == 6) {
+                        $this->db->where('posisi_id', $karyawan['atasan2']);
+                        $this->db->where('sect_id', $karyawan['sect_id']);
+                        $this->db->where('is_active', '1');
+                        $atasan2 = $this->db->get('karyawan')->row_array();
+                    };
+
+                    if ($karyawan['dept_id'] == '11' and $karyawan['atasan2'] == '3') {
+                        $atasan2 = $this->db->get_where('karyawan', ['inisial' => 'ABU'])->row_array();
+                    }
+
+                    if ($karyawan['dept_id'] == '14' and $karyawan['atasan2'] == '3') {
+                        $atasan2 = $this->db->get_where('karyawan', ['inisial' => 'KKO'])->row_array();
+                    }
+
+                    if ($karyawan['npk'] == '1111') {
+                        $atasan1 = $this->db->get_where('karyawan', ['inisial' => 'RAISA'])->row_array();
+                        $atasan2 = $this->db->get_where('karyawan', ['inisial' => 'RAISA'])->row_array();
+                    }
+
+                    $data = [
+                        'npk' => $karyawan['npk'],
+                        'inisial' => $karyawan['inisial'],
+                        'nama' => $karyawan['nama'],
+                        'gol_id' => $karyawan['gol_id'],
+                        'posisi_id' => $karyawan['posisi_id'],
+                        'div_id' => $karyawan['div_id'],
+                        'dept_id' => $karyawan['dept_id'],
+                        'sect_id' => $karyawan['sect_id'],
+                        'atasan1' => $atasan1['npk'],
+                        'atasan1_inisial' => $atasan1['inisial'],
+                        'atasan2' => $atasan2['npk'],
+                        'atasan2_inisial' => $atasan2['inisial'],
+                        'contract' => $karyawan['work_contract'],
+                        'role_id' => $karyawan['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    $this->session->set_flashdata('message', 'masuk');
+
+                    $log = [
+                        'npk' => $karyawan['npk'],
+                        'activity' => 'Login to RAISA'
+                    ];
+                    $this->db->insert('log', $log);
+
+                    $this->db->set('last_login', date('Y-m-d H:i:s'));
+                    $this->db->where('npk', $this->input->post('npk'));
+                    $this->db->update('karyawan');
+
+                    redirect('dashboard');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-rose">
+                    <strong>Login Gagal</strong>
+                    <span>Maaf, Password yang kamu masukan salah.</span>
+                    </div> </br>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-rose">
+                <strong>Login Gagal</strong>
+                <span>Maaf, NPK Kamu tidak ditemukan.</span>
+                </div> </br>');
+                redirect('auth');
+            }
+        }
+    }
+
 }
