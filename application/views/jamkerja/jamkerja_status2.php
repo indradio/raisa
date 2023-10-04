b403223cff3d5f455fe147ef8b1bd69370e76a76
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2023-10-04 14:55:13 +0700

    Update jamkerja_status2.php

--- application/views/jamkerja/jamkerja_status2.php
+++ application/views/jamkerja/jamkerja_status2.php
@@ -100,6 +100,7 @@
                   for ($i = 1; $i < $tanggal + 1; $i++) {
                     $this->db->where('is_active', '1');
                     $this->db->where('dept_id', '11');
+                    $this->db->where('sect_id', '111');
                     $kry = $this->db->get_where('karyawan', ['work_contract' => 'Direct Labor'])->result_array();
                     foreach ($kry as $k) :
                       $this->db->where('npk', $k['npk']);
