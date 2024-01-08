384b96a77a9b55f5e648e3f3899a9f25a7da666e
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2024-01-08 08:51:31 +0700

    Update reservasi_list.php

--- application/views/perjalanan/reservasi_list.php
+++ application/views/perjalanan/reservasi_list.php
@@ -128,7 +128,7 @@
                                     <?php
                                     foreach ($cancelled as $row) : ?>
                                         <?php if ($row['tglberangkat'] < date('Y-m-d')) {
-                                            echo '<tr class="text-light bg-danger">';
+                                            echo '<tr class="text-white bg-danger">';
                                             } else {
                                             echo '<tr>';
                                             } ?>
@@ -158,7 +158,7 @@
                                     <?php
                                     foreach ($cancelled_ta as $row) : ?>
                                         <?php if ($row['tglberangkat'] < date('Y-m-d')) {
-                                            echo '<tr class="text-dark bg-danger">';
+                                            echo '<tr class="text-white bg-danger">';
                                             } else {
                                             echo '<tr>';
                                             } ?>
