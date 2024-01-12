1ab14b6844ef3279dd3155b465804bd0df81ea0c
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2024-01-12 11:57:21 +0700

    Update index.php

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -64,38 +64,38 @@
           </div>
       <?php endforeach; ?>
       <div class="col-md-0 ml-auto mr-auto d-block d-sm-none">
-          <div class="card">
+          <div class="card bg-transparent">
               <div class="card-body">
                   <div class="row">
-                    <div class="col-3 text-center" style="padding-left: 1%;padding-right: 1%">
+                    <div class="col-4 text-center" style="padding-left: 1%;padding-right: 1%">
                         <a href="<?= base_url('presensi'); ?>" class="btn btn-lg btn-just-icon btn-success">
                             <i class="fa fa-street-view"></i>
                         </a>
                         </br>
                         <a class="card-title"><small>ABSEN</small></a> 
                       </div>
-                      <div class="col-3 text-center"  style="padding-left: 1%;padding-right: 1%">
+                      <div class="col-4 text-center"  style="padding-left: 1%;padding-right: 1%">
                           <a href="<?= base_url('lembur'); ?>" class="btn btn-lg btn-just-icon btn-facebook">
                               <i class="fa fa-clock-o"></i>
                           </a>
                           </br>
                           <a class="card-title"><small>OT</small></a> 
                       </div>
-                      <div class="col-3 text-center"  style="padding-left: 1%;padding-right: 1%">
+                      <div class="col-4 text-center"  style="padding-left: 1%;padding-right: 1%">
                           <a href="<?= base_url('perjalanandl'); ?>" class="btn btn-lg btn-just-icon btn-facebook">
                               <i class="fa fa-car"></i>
                           </a>
                           </br>
                           <a class="card-title"><small>DL</small></a> 
                       </div>
-                      <div class="col-3 text-center"  style="padding-left: 1%;padding-right: 1%">
+                      <div class="col-4 text-center"  style="padding-left: 1%;padding-right: 1%">
                           <a href="<?= base_url('imp'); ?>" class="btn btn-lg btn-just-icon btn-facebook">
                               <i class="fa fa-plane"></i>
                           </a>
                           </br>
                           <a class="card-title"><small>IMP</small></a> 
                       </div>
-                      <div class="col-3 mr-auto text-center"  style="padding-left: 1%;padding-right: 1%">
+                      <div class="col-4 mr-auto text-center"  style="padding-left: 1%;padding-right: 1%">
                           <a href="<?= base_url('cuti'); ?>" class="btn btn-lg btn-just-icon btn-facebook">
                               <i class="fa fa-calendar-times-o"></i>
                           </a>
@@ -596,25 +596,25 @@
     let timerInterval
     <?php if ($this->session->flashdata('message')=='masuk'){ ?> 
       
-        // Swal.fire({
-        //   title: 'Selamat Hari Raya Idul Fitri 1443H',
-        //   html: '',
-        //   imageUrl: '<?= base_url(); ?>/assets/img/info/idul-fitri-1443H.jpg',
-        //   imageWidth: 400,
-        //   imageHeight: 200,
-        //   imageAlt: 'Custom image',
-        //   timer: 5000,
-        //   timerProgressBar: true,
-        //   showConfirmButton: false,
-        //   willClose: () => {
-        //     clearInterval(timerInterval)
-        //   }
-        // }).then((result) => {
-        //   /* Read more about handling dismissals below */
-        //   if (result.dismiss === Swal.DismissReason.timer) {
-        //     console.log('I was closed by the timer')
-        //   }
-        // });
+        Swal.fire({
+          title: 'Untuk melihat perjalanan hari ini silahkan ke menu Perjalanan Dinas lalu pilih "Sedang Bertugas"',
+          html: '',
+          // imageUrl: '<?= base_url(); ?>/assets/img/info/idul-fitri-1443H.jpg',
+          // imageWidth: 400,
+          // imageHeight: 200,
+          // imageAlt: 'Custom image',
+          timer: 5000,
+          timerProgressBar: true,
+          showConfirmButton: false,
+          willClose: () => {
+            clearInterval(timerInterval)
+          }
+        }).then((result) => {
+          /* Read more about handling dismissals below */
+          if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+          }
+        });
        
      <?php }; ?>
 
@@ -652,14 +652,6 @@
       ],
     });
 
-
-    $('#hapusClaim').on('show.bs.modal', function(event) {
-      var button = $(event.relatedTarget) // Button that triggered the modal
-      var id = button.data('id') // Extract info from data-* attributes
-      var modal = $(this)
-      modal.find('.modal-body input[name="id"]').val(id)
-    });
-
     $('#bannerModal').on('show.bs.modal', function(event) {
       var div = $(event.relatedTarget) // Tombol dimana modal di tampilkan
       var modal = $(this)
@@ -670,157 +662,7 @@
       $(".alert").fadeTo(200, 0).slideUp(200, function() {
         $(this).remove();
       });
-    }, 60000);
-
-    // $('#detail').on('show.bs.modal', function(event) {
-    //   var button = $(event.relatedTarget) // Button that triggered the modal
-    //   var id = button.data('id') // Extract info from data-* attributes
-    //   var modal = $(this)
-    //   modal.find('.modal-body input[name="device_id"]').val(id)
-
-    // setInterval(function() {
-
-      const cars = ["2020080159", "2020080160", "2020080161", "2020080162", "2020080163"];
-
-      for (let i = 0; i < cars.length; i++) {
-
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", 'https://gps.intellitrac.co.id/apis/tracking/realtime.php', true);
-
-        //Send the proper header information along with the request
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-        xhr.onreadystatechange = function() { // Call a function when the state changes.
-          if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            var myObj = JSON.parse(this.responseText);
-            var image = 'https://raisa.winteq-astra.com/assets/img/iconmobil.png';
-            var idloc = "loc" + cars[i];
-            var lat = "lat" + cars[i];
-            var lng = "lng" + cars[i]; 
-            
-            loc = myObj.data[cars[i]]['realtime']['location'];
-            // z + cars[i] = myObj.data[cars[i]]['realtime']['ignition_status'];
-            lat = myObj.data[cars[i]]['realtime']['latitude'];
-            lng = myObj.data[cars[i]]['realtime']['longitude'];
-            
-            var mapCanvas = document.getElementById(cars[i]);
-            document.getElementById(idloc).innerHTML = loc;
-            
-            var location = new google.maps.LatLng(lat, lng);
-            var mapOptions = {
-              center: location,
-              zoom: 15,
-              mapTypeId: "satellite",
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
-            var map = new google.maps.Map(mapCanvas, mapOptions);
-
-            var marker = new google.maps.Marker({
-              position: location,
-              icon: image
-            });
-            
-            marker.setMap(map);
-          }
-        }
-      xhr.send("username=winteq&password=winteq1231407&devices=2020080159%3B2020080160%3B2020080161%3B2020080162%3B2020080163");
-      }
-
-    // }, 5000);
-
-    var checker = document.getElementById('check');
-    var sendbtn = document.getElementById('submit');
-    sendbtn.disabled = true;
-
-    // when unchecked or checked, run the function
-    checker.onchange = function() {
-      if (this.checked) {
-        sendbtn.disabled = false;
-      } else {
-        sendbtn.disabled = true;
-      }
-    }
-
-    var cJamkerja = $('#calendarJamkerja');
-
-    today = new Date();
-    y = today.getFullYear();
-    m = today.getMonth();
-    d = today.getDate();
-
-    cJamkerja.fullCalendar({
-      timeZone: 'asia/jakarta', // the default (unnecessary to specify)
-      viewRender: function(view, element) {
-        // We make sure that we activate the perfect scrollbar when the view isn't on Month
-        if (view.name != 'month') {
-          $(element).find('.fc-scroller').perfectScrollbar();
-        }
-      },
-      header: {
-        left: 'month',
-        center: 'title',
-        right: 'prev,next,today'
-      },
-
-      firstDay: 1,
-      defaultDate: today,
-      businessHours: [{
-        default: false,
-        // days of week. an array of zero-based day of week integers (0=Sunday)
-        dow: [1, 2, 3, 4, 5], // Monday - Friday
-        start: '07:30', // a start time 
-        end: '16:30' // an end time 
-      }],
-
-      views: {
-        month: { // name of view
-          titleFormat: 'MMMM YYYY'
-          // other view-specific options here
-        },
-        week: {
-          titleFormat: " MMMM D YYYY"
-        },
-        day: {
-          titleFormat: 'D MMM, YYYY'
-        }
-      },
-
-      selectable: true,
-      selectHelper: true,
-      editable: true,
-
-      eventSources: [{
-          events: function(start, end, timezone, callback) {
-            $.ajax({
-              url: '<?php echo base_url() ?>jamkerja/GET_MY_jamkerja',
-              dataType: 'json',
-              success: function(msg) {
-                var events = msg.events;
-                callback(events);
-              }
-            });
-          }
-        },
-        {
-          events: function(start, end, timezone, callback) {
-            $.ajax({
-              url: '<?php echo base_url() ?>jamkerja/GET_MY_lembur',
-              dataType: 'json',
-              success: function(msg) {
-                var events = msg.events;
-                callback(events);
-              }
-            });
-          },
-          color: 'red' // an option!
-        },
-      ],
-      eventLimit: true, // allow "more" link when too many events
-
-      select: function(start, end, info) {
-        window.location = 'https://raisa.winteq-astra.com/jamkerja/tanggal/' + start.format();
-      },
-    });    
+    }, 60000); 
 
   });
 </script>
