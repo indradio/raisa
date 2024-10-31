3eae6fbbe06b3177960fb980ea9cae700cc31c29
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2024-10-31 10:24:59 +0700

    Update Jamkerja.php

--- application/controllers/Jamkerja.php
+++ application/controllers/Jamkerja.php
@@ -6,7 +6,7 @@ class Jamkerja extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        is_logged_in();
+        // is_logged_in();
         date_default_timezone_set('asia/jakarta');
         
         $this->load->model("jamkerja_model");
