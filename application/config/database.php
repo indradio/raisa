91900230adbc72643b947675a0978e891bc08d38
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2025-03-26 18:10:26 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -72,29 +72,3 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
-$db['default'] = array(
-	'dsn'	=> '',
-	// 'hostname' => '116.204.248.224',
-	// 'username' => 'cpwinteq_root',
-	// 'password' => 'admin',
-	// 'database' => 'cpwinteq_raisa',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'db_raisa',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
