99e6b8d9a456a20cddcbe995b4fb266b2719e75e
Indra Dio Aji Prayoga <51142501+indradio@users.noreply.github.com>
2025-08-29 13:09:31 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -111,52 +111,52 @@
 <script src="<?= base_url(); ?>assets/js/core/popper.min.js"></script>
 <script src="<?= base_url(); ?>assets/js/core/bootstrap-material-design.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js" async></script>
-<script src="<?= base_url(); ?>assets/js/plugins/perfect-scrollbar.jquery.min.js" async></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
 <!-- Plugin for the momentJs  -->
-<script src="<?= base_url(); ?>assets/js/plugins/moment.min.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/moment.min.js"></script>
 <!--  Plugin for Sweet Alert -->
-<script src="<?= base_url(); ?>assets/js/plugins/sweetalert2.js" async></script>
-<script src="<?= base_url(); ?>assets/js/plugins/sweetalert2.min.js" async></script>
-<script src="<?= base_url(); ?>assets/js/plugins/sweet-alert.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/sweetalert2.js"></script>
+<script src="<?= base_url(); ?>assets/js/plugins/sweetalert2.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/plugins/sweet-alert.js"></script>
 <!-- Forms Validations Plugin -->
-<script src="<?= base_url(); ?>assets/js/plugins/jquery.validate.min.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/jquery.validate.min.js"></script>
 <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
-<script src="<?= base_url(); ?>assets/js/plugins/jquery.bootstrap-wizard.js" defer></script>
+<script src="<?= base_url(); ?>assets/js/plugins/jquery.bootstrap-wizard.js"></script>
 <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
 <script src="<?= base_url(); ?>assets/js/plugins/bootstrap-selectpicker.js"></script>
 <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
 <script src="<?= base_url(); ?>assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
 <!-- Plugin for the Modal  -->
-<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-modal.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-modal.js"></script>
 <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
 <script src="<?= base_url(); ?>assets/js/plugins/jquery.dataTables.min.js"></script>
 <script src="<?= base_url(); ?>assets/js/plugins/dataTables.js"></script>
 <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-tagsinput.js" defer></script>
+<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-tagsinput.js"></script>
 <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-<script src="<?= base_url(); ?>assets/js/plugins/jasny-bootstrap.min.js" defer></script>
+<script src="<?= base_url(); ?>assets/js/plugins/jasny-bootstrap.min.js"></script>
 <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-<script src="<?= base_url(); ?>assets/js/plugins/fullcalendar.min.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/fullcalendar.min.js"></script>
 <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
-<script src="<?= base_url(); ?>assets/js/plugins/jquery-jvectormap.js" defer></script>
+<script src="<?= base_url(); ?>assets/js/plugins/jquery-jvectormap.js"></script>
 <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-<script src="<?= base_url(); ?>assets/js/plugins/nouislider.min.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/nouislider.min.js"></script>
 <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js" async></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
 <!-- Library for adding dinamically elements -->
-<script src="<?= base_url(); ?>assets/js/plugins/arrive.min.js" defer></script>
+<script src="<?= base_url(); ?>assets/js/plugins/arrive.min.js"></script>
 <!--  Google Maps Plugin    -->
 <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHFISdyofTP6NPRE142yGJjZPa1Z2VbU4"></script> -->
 <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA-ubIYpWmP5j_UGlt6B4xzUsjASRsmeo0"></script> -->
 <!-- Chartist JS -->
-<script src="<?= base_url(); ?>assets/js/plugins/chartist.min.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/chartist.min.js"></script>
 <!--  Notifications Plugin    -->
-<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-notify.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="<?= base_url(); ?>assets/js/material-dashboard.js?v=2.1.0" type="text/javascript" async></script>
+<script src="<?= base_url(); ?>assets/js/material-dashboard.js?v=2.1.0" type="text/javascript"></script>
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="<?= base_url(); ?>assets/js/plugins/simple.money.format.js" async></script>
+<script src="<?= base_url(); ?>assets/js/plugins/simple.money.format.js"></script>
 
 <script>
     $(document).ready(function() {
@@ -329,6 +329,7 @@
         });
     });
 </script>
+
 <!-- javascript for init -->
 <script>
     $('.datetimepicker').datetimepicker({
